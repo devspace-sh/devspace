39cf82dbe71213891b20c90b4b228a7c58b3a91b
Florian <1522784@stud.hs-mannheim.de>
2018-08-21 11:11:10 +0200

    New Basic Test for these juju/errors

---
+++ pkg/util/jujuerr_test.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"testing"
+	jujuerr "github.com/juju/errors"
+)
+
+func TestJuju(t *testing.T) {
+	err := funcThatCallsFailingFunc()
+
+	if err == nil {
+		t.Fail()
+	} else {
+		t.Log("Error: " + err.Error())
+		t.Log("Cause: " + jujuerr.Cause(err).Error())
+		t.Log("Details: " + jujuerr.Details(err))
+		t.Log("StackTrace: " + jujuerr.ErrorStack(err))
+	}
+}
+
+func funcThatCallsFailingFunc() error{
+	err := failingFunc()
+	return jujuerr.Trace(err)
+}
+
+func failingFunc() error {
+	return jujuerr.New("Some error")
+}
