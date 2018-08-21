99d085768df4c69275c928ad14ffe1d4573635d3
Jerlam <J$r$m061290>
2018-08-21 16:02:00 +0200

    colored console output

--- .history/examples/nodejs/index_20180821152104.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-mai;
-app.get('/', (req, res) => res.send('Hello World12345!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- examples/nodejs/index.js
+++ examples/nodejs/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World12345!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
