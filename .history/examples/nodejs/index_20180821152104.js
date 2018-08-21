9ba07b9b3105e3aca8f84c552406f546fbe47b33
Jerlam <J$r$m061290>
2018-08-21 15:50:33 +0200

    colored console output

---
+++ .history/examples/nodejs/index_20180821152104.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+mai;
+app.get('/', (req, res) => res.send('Hello World12345!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ .history/pkg/util/logutil/stdout_20180821151829.go
@@ -0,0 +1,41 @@
+package logutil
+
+import (
+	"os"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+	ct "github.com/daviddengcn/go-colortext"
+)
+
+type TerminalHook struct {
+	LogLevels []logrus.Level
+}
+
+func (hook TerminalHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
+
+func (hook TerminalHook) Fire(entry *logrus.Entry) error {
+	err, hasErr := entry.Data[logrus.ErrorKey]
+	message := "[" + strings.ToUpper(entry.Level.String()) + "] " + entry.Message + "\n"
+
+	if hasErr {
+		ct.Foreground(ct.Green, false)
+		ct.ChangeColor(ct.Red, true, ct.White, false)
+		errCasted := err.(error)
+		message = message + errCasted.Error() + "\n"
+		ct.ResetColor()
+	}
+	output := []byte(message)
+
+	if entry.Level == logrus.InfoLevel {
+		ct.Foreground(ct.Green, false)
+		// ct.ChangeColor(ct.Red, true, ct.White, false)
+		os.Stdout.Write(output)
+		ct.ResetColor()
+	} else {
+		os.Stderr.Write(output)
+	}
+	return nil
+}
