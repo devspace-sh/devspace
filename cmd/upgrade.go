0a5c228fe5f8ce7c062e76197dabce7668461924
Fabian Kramm <kramm@covexo.com>
2018-08-22 18:43:25 +0200

    Implement Upgrade cmd

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -1,1 +1,40 @@
 package cmd
+
+import (
+	"github.com/covexo/devspace/pkg/devspace/upgrade"
+	"github.com/spf13/cobra"
+)
+
+type UpgradeCmd struct {
+	flags *UpgradeCmdFlags
+}
+
+type UpgradeCmdFlags struct {
+}
+
+func init() {
+	cmd := &UpgradeCmd{
+		flags: &UpgradeCmdFlags{},
+	}
+
+	cobraCmd := &cobra.Command{
+		Use:   "upgrade",
+		Short: "Upgrade the devspace cli to the newest version",
+		Long: `
+#######################################################
+################## devspace upgrade ###################
+#######################################################
+Upgrades the devspace cli to the newest version
+#######################################################`,
+		Run: cmd.Run,
+	}
+	rootCmd.AddCommand(cobraCmd)
+}
+
+func (cmd *UpgradeCmd) Run(cobraCmd *cobra.Command, args []string) {
+	err := upgrade.Upgrade()
+
+	if err != nil {
+		log.Fatalf("Couldn't upgrade: %s\n", err.Error())
+	}
+}
