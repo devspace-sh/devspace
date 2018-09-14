fae6b3cea1599849a9f6790a5ad789bcddf99f73
Fabian Kramm <kramm@covexo.com>
2018-09-14 10:55:54 +0200

    Remove history size

--- scripts/coverage.bash
+++ scripts/coverage.bash
@@ -11,7 +11,7 @@ go build main.go
 
 PKGS=$(go list ./... | grep -v /vendor/)
 for pkg in $PKGS; do
-  GORACE="history_size=7" go test -race -coverprofile=profile.out -covermode=atomic $pkg
+  go test -race -coverprofile=profile.out -covermode=atomic $pkg
   if [[ -f profile.out ]]; then
     cat profile.out >> coverage.txt
     rm profile.out
