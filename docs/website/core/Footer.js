6d9eee9f81b32a1e61e237b4ec754cd3bd672e4d
Lukas Gentele <gentele@covexo.com>
2019-02-15 19:54:21 -0800

    setup new docs page

--- docs/website/core/Footer.js
+++ docs/website/core/Footer.js
@@ -10,88 +10,20 @@ const React = require('react');
 class Footer extends React.Component {
   docUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + 'docs/' + doc;
+    const docsUrl = this.props.config.docsUrl;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    return `${baseUrl}${docsPart}${langPart}${doc}`;
   }
 
   pageUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? language + '/' : '') + doc;
+    return baseUrl + (language ? `${language}/` : '') + doc;
   }
 
   render() {
-    const currentYear = new Date().getFullYear();
     return (
       <footer className="nav-footer" id="footer">
-        <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-                height="58"
-              />
-            )}
-          </a>
-          <div>
-            <h5>Docs</h5>
-            <a href={this.docUrl('getting-started/faq.html', this.props.language)}>
-              Frequently Asked Questions (FAQ)
-            </a>
-            <a href={this.docUrl('getting-started/quickstart.html', this.props.language)}>
-              Getting Started (Quickstart Guide)
-            </a>
-            <a href={this.docUrl('cli/init.html', this.props.language)}>
-              Command Line Interface (CLI Docs)
-            </a>
-            <a href={this.docUrl('configuration/dockerfile.html', this.props.language)}>
-              Configuration Files
-            </a>
-          </div>
-          <div>
-            <h5>Community</h5>
-            <a
-              href="http://stackoverflow.com/questions/tagged/devspace"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
-            </a>
-            <a
-              href="https://twitter.com/covexo"
-              target="_blank"
-              rel="noreferrer noopener">
-              Twitter
-            </a>
-          </div>
-          <div>
-            <h5>More</h5>
-            <a href="https://github.com/covexo/devspace">GitHub</a>
-            <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/covexo/devspace/stargazers"
-              data-show-count={true}
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
-              Star
-            </a>
-          </div>
-        </section>
-
-        <a
-          href="https://github.com/covexo/devspace/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="footer-logo">
-          <img
-            src={this.props.config.baseUrl + 'img/devspace-logo.svg'}
-            alt="DevSpace CLI Logo"
-            width="300"
-            height="54"
-          />
-        </a>
-        <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
   }
--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -5,59 +5,50 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// See https://docusaurus.io/docs/site-config.html for all the possible
+// See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-/* List of projects/orgs using your project for the users page */
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
-  title: 'DevSpace Documentation' /* title for your website */,
-  tagline: 'Cloud-Native Development with Kubernetes',
-  url: 'https://docs.devspace-cloud.com' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  title: 'DevSpace Documentation (CLI, Cloud)', // Title for your website.
+  tagline: 'A website for testing',
+  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'devspace',
-  organizationName: 'covexo',
+  projectName: 'test-site',
+  organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'getting-started/quickstart', label: 'Docs'},
-    {page: 'help', label: 'Help'}
+    {href: 'http://devspace.cloud/products', label: 'Products'},
+    {doc: 'getting-started/installation', label: 'Docs'},
+    {href: 'http://devspace.cloud/', label: 'Help'},
   ],
 
-  // If you have users set above, you add it here:
-  users,
-
   /* path to images for header/footer */
-  headerIcon: 'img/devspace-logo.svg',
-  footerIcon: 'img/devspace-icon.svg',
-  favicon: 'img/devspace-favicon.png',
+  headerIcon: 'img/docusaurus.svg',
+  footerIcon: 'img/docusaurus.svg',
+  favicon: 'img/favicon.png',
 
-  /* colors for website */
+  // Open Graph and Twitter card images.
+  ogImage: 'img/docusaurus.png',
+  twitterImage: 'img/docusaurus.png',
+
+  /* Colors for website */
   colors: {
-    primaryColor: '#233366',
-    secondaryColor: '#ff8800',
+    primaryColor: '#223366',
+    secondaryColor: '#00BDFF',
   },
 
-  /* custom fonts for website */
-  /*fonts: {
+  /* Custom fonts for website */
+  /*
+  fonts: {
     myFont: [
       "Times New Roman",
       "Serif"
@@ -66,32 +57,36 @@ const siteConfig = {
       "-apple-system",
       "system-ui"
     ]
-  },*/
+  },
+  */
 
-  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
-  copyright:
-    'Copyright © ' +
-    new Date().getFullYear() +
-    ' covexo GmbH',
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} DevSpace`,
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks
+    // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: 'default',
   },
 
-  // Add custom scripts here that would be placed in <script> tags
-  scripts: ['https://buttons.github.io/buttons.js'],
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: [],
 
-  /* On page navigation for the current documentation page */
+  // On page navigation for the current documentation page.
   onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
 
-  /* Open Graph and Twitter card images */
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  enableUpdateTime: true,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
+
+  //customDocsPath: 'pages',
 };
 
 module.exports = siteConfig;
