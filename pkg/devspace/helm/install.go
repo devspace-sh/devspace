a6410272034bfc9a2ea1954c7eb4d8ff7ab9c277
fabiankramm <kramm@covexo.com>
2019-02-27 11:25:25 -0800

    Run analyze on helm deploy timeout

--- pkg/devspace/helm/install.go
+++ pkg/devspace/helm/install.go
@@ -1,10 +1,14 @@
 package helm
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
+
+	"github.com/devspace-cloud/devspace/pkg/devspace/analyze"
+	"github.com/devspace-cloud/devspace/pkg/devspace/kubectl"
 
 	"github.com/devspace-cloud/devspace/pkg/devspace/config/configutil"
 	"github.com/devspace-cloud/devspace/pkg/util/log"
@@ -19,6 +23,9 @@ import (
 	"k8s.io/helm/pkg/proto/hapi/chart"
 	hapi_release5 "k8s.io/helm/pkg/proto/hapi/release"
 )
+
+// DeploymentTimeout is the timeout to wait for helm to deploy
+const DeploymentTimeout = int64(2 * 60)
 
 func checkDependencies(ch *chart.Chart, reqs *helmchartutil.Requirements) error {
 	missing := []string{}
@@ -90,7 +97,6 @@ func (helmClientWrapper *ClientWrapper) InstallChartByPath(releaseName, releaseN
 		return nil, err
 	}
 
-	deploymentTimeout := int64(10 * 60)
 	overwriteValues := []byte("")
 
 	if values != nil {
@@ -106,7 +112,7 @@ func (helmClientWrapper *ClientWrapper) InstallChartByPath(releaseName, releaseN
 		upgradeResponse, err := helmClientWrapper.Client.UpdateRelease(
 			releaseName,
 			chartPath,
-			k8shelm.UpgradeTimeout(deploymentTimeout),
+			k8shelm.UpgradeTimeout(DeploymentTimeout),
 			k8shelm.UpdateValueOverrides(overwriteValues),
 			k8shelm.ReuseValues(false),
 			k8shelm.UpgradeForce(true),
@@ -114,13 +120,18 @@ func (helmClientWrapper *ClientWrapper) InstallChartByPath(releaseName, releaseN
 		)
 
 		if err != nil {
-			log.Warn("Try to roll back back chart because of previous error")
-			_, rollbackError := helmClientWrapper.Client.RollbackRelease(releaseName, k8shelm.RollbackTimeout(180))
-			if rollbackError != nil {
-				return nil, fmt.Errorf("Error deploying release %s: %v\nRun `%s` to force delete the chart. Warning: purging will also delete in the chart defined persistent volume claims", releaseName, err, ansi.Color("devspace purge", "white+b"))
+			err = helmClientWrapper.analyzeError(fmt.Errorf("helm upgrade: %v", err), releaseNamespace)
+			if err != nil {
+				log.Warn("Try to roll back back chart because of previous error")
+				_, rollbackError := helmClientWrapper.Client.RollbackRelease(releaseName, k8shelm.RollbackTimeout(180))
+				if rollbackError != nil {
+					return nil, fmt.Errorf("Error deploying release %s: %v\nRun `%s` to force delete the chart. Warning: purging will also delete in the chart defined persistent volume claims", releaseName, err, ansi.Color("devspace purge", "white+b"))
+				}
+
+				return nil, err
 			}
 
-			return nil, err
+			return nil, nil
 		}
 
 		return upgradeResponse.GetRelease(), nil
@@ -129,20 +140,52 @@ func (helmClientWrapper *ClientWrapper) InstallChartByPath(releaseName, releaseN
 	installResponse, err := helmClientWrapper.Client.InstallReleaseFromChart(
 		chart,
 		releaseNamespace,
-		k8shelm.InstallTimeout(deploymentTimeout),
+		k8shelm.InstallTimeout(DeploymentTimeout),
 		k8shelm.ValueOverrides(overwriteValues),
 		k8shelm.ReleaseName(releaseName),
 		k8shelm.InstallReuseName(true),
 		k8shelm.InstallWait(wait),
 	)
 	if err != nil {
-		// Try to delete and ignore errors, because otherwise we have a broken release laying around and always get the no deployed resources error
-		helmClientWrapper.DeleteRelease(releaseName, true)
+		err = helmClientWrapper.analyzeError(fmt.Errorf("helm install: %v", err), releaseNamespace)
+		if err != nil {
+			// Try to delete and ignore errors, because otherwise we have a broken release laying around and always get the no deployed resources error
+			helmClientWrapper.DeleteRelease(releaseName, true)
 
-		return nil, err
+			return nil, err
+		}
+
+		return nil, nil
 	}
 
 	return installResponse.GetRelease(), nil
+}
+
+// analyzeError calls analyze and tries to find the issue
+func (helmClientWrapper *ClientWrapper) analyzeError(srcErr error, releaseNamespace string) error {
+	errMessage := srcErr.Error()
+
+	// Only check if the error is time out
+	if strings.Index(errMessage, "timed out waiting") != -1 {
+		config, err := kubectl.GetClientConfig()
+		if err != nil {
+			log.Warnf("Error loading kubectl config: %v", err)
+			return srcErr
+		}
+
+		report, err := analyze.CreateReport(helmClientWrapper.kubectl, config, releaseNamespace, false)
+		if err != nil {
+			log.Warnf("Error creating analyze report: %v", err)
+			return srcErr
+		}
+		if len(report) == 0 {
+			return nil
+		}
+
+		return errors.New(analyze.ReportToString(report))
+	}
+
+	return srcErr
 }
 
 // InstallChartByName installs the given chart by name under the releasename in the releasenamespace
