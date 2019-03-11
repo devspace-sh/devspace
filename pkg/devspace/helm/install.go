61c80d479443a97ba82c564ca86be768dfb341d0
fabiankramm <kramm@covexo.com>
2019-03-10 17:51:40 -0700

    Change helm update fail message

--- pkg/devspace/helm/install.go
+++ pkg/devspace/helm/install.go
@@ -136,7 +136,7 @@ func (helmClientWrapper *ClientWrapper) InstallChartByPath(releaseName, releaseN
 				log.Warn("Try to roll back back chart because of previous error")
 				_, rollbackError := helmClientWrapper.Client.RollbackRelease(releaseName, k8shelm.RollbackTimeout(180))
 				if rollbackError != nil {
-					return nil, fmt.Errorf("Error deploying release %s: %v\nRun `%s` to delete the space", releaseName, err, ansi.Color("devspace purge", "white+b"))
+					return nil, fmt.Errorf("Error deploying release %s: %v\nRun `%s` to force the deletion of the space", releaseName, err, ansi.Color("devspace purge", "white+b"))
 				}
 
 				return nil, err
