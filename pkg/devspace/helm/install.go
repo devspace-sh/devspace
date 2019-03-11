e1fd90feb41a583de34914bfc8bb9504096e444a
fabiankramm <kramm@covexo.com>
2019-03-10 17:49:37 -0700

    Change error message on helm upgrade

--- pkg/devspace/helm/install.go
+++ pkg/devspace/helm/install.go
@@ -136,7 +136,7 @@ func (helmClientWrapper *ClientWrapper) InstallChartByPath(releaseName, releaseN
 				log.Warn("Try to roll back back chart because of previous error")
 				_, rollbackError := helmClientWrapper.Client.RollbackRelease(releaseName, k8shelm.RollbackTimeout(180))
 				if rollbackError != nil {
-					return nil, fmt.Errorf("Error deploying release %s: %v\nRun `%s` to force delete the chart. Warning: purging will also delete in the chart defined persistent volume claims", releaseName, err, ansi.Color("devspace purge", "white+b"))
+					return nil, fmt.Errorf("Error deploying release %s: %v\nRun `%s` to delete the space", releaseName, err, ansi.Color("devspace purge", "white+b"))
 				}
 
 				return nil, err
