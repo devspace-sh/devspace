172aed7e1bebb848c92747856f2fab2c1b17766b
Lukas Gentele <gentele@covexo.com>
2019-03-11 09:11:43 -0700

    fix issue with line numbers for code blocks

--- docs/website/static/js/code-block-line-numbers.js
+++ docs/website/static/js/code-block-line-numbers.js
@@ -2,14 +2,16 @@ window.addEventListener('load', function() {
     var el = document.getElementsByClassName('hljs')
     
     for (let i = 0; i < el.length; i++) {
-        var lineNumber = 0
+        var lineNumber = 0;
+        var lineId;
         
         el[i].innerHTML = el[i].innerHTML.replace(/(^|\n)/g, function() {
             lineNumber++;
-            var lineId = 'block-' + i + '-line-' + lineNumber
+            lineId = 'block-' + i + '-line-' + lineNumber
             return arguments[1] + '<a class="line" href="#' + lineId + '" id="' + lineId + '">' + lineNumber + '</a>';
         })
+        var lastLine = document.getElementById(lineId);
 
-        el[i].removeChild(el[i].lastChild)
+        lastLine.parentElement.removeChild(lastLine);
     }
 });
