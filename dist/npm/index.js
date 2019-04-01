af88724c784770a90456c5ac62fee8eb5d47ef1b
Lukas Gentele <gentele@covexo.com>
2019-04-01 15:43:25 -0700

    add noop test script

--- dist/npm/index.js
+++ dist/npm/index.js
@@ -4,6 +4,12 @@ var path = require('path');
 var exec = require('child_process').exec;
 var request = require('request');
 
+
+if (action == "noop") {
+    console.log("Successfully ran noop command");
+    process.exit(0);
+}
+
 const downloadPathTemplate = "https://github.com/devspace-cloud/devspace/releases/download/v{{version}}/devspace-{{platform}}-{{arch}}";
 const ARCH_MAPPING = {
     "ia32": "386",
