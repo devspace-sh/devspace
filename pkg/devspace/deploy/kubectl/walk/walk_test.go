6ff4cd930edf06221d9d268ff47f575751b4adc2
Florian <1522784@stud.hs-mannheim.de>
2019-05-02 15:48:37 +0200

    Walk_Test implemented

--- pkg/devspace/deploy/kubectl/walk/walk_test.go
+++ pkg/devspace/deploy/kubectl/walk/walk_test.go
@@ -1,34 +1,63 @@
 package walk
 
-import "testing"
+import (
+	"testing"
+	"gotest.tools/assert" 
+"gopkg.in/yaml.v2"
+)
 
 func TestWalk(t *testing.T) {
 	// @Florian
 	// Take a look at how the walk function works and transform the below input into the output below
 
 	// Input yaml
-	_ = `
-test: 123
-	image: appendtag
-	test: []
+	input := `
+test:
+  image: appendtag
+  test: []
 test2:
-	image: dontreplaceme
-	test3:
-	- test4:
-		test5:
-		image: replaceme
-	`
+  image: dontreplaceme
+  test3:
+  - test4:
+      test5:
+      image: replaceme
+`
+	inputObj := make(map[interface{}]interface{})
+	err := yaml.Unmarshal([]byte(input), inputObj)
+	if err != nil {
+		t.Fatalf("Error parsing input: %v", err)
+	}
+
+	match := MatchFn(func(path, key, value string) bool{
+		return key == "image" && value != "dontreplaceme"
+	})
+	replace := ReplaceFn(func(path, value string) (interface{}){
+		if value == "appendtag" {
+			return "appendtag:test"
+		}else {
+			return "replaced"
+		}
+	})
+
+	Walk(inputObj, match, replace)
+
+	output, err := yaml.Marshal(inputObj)
+	if err != nil {
+		t.Fatalf("Error parsing output: %v", err)
+	}
 
 	// Output yaml
-	_ = `
-test: 123
-	image: appendtag:test
-	test: []
+	expected := `test:
+  image: appendtag:test
+  test: []
 test2:
-	image: dontreplaceme
-	test3:
-	- test4:
-		test5:
-		image: replaced
-		`
+  image: dontreplaceme
+  test3:
+  - test4:
+      image: replaced
+      test5: null
+`
+
+		t.Log(string(output))
+	assert.Equal(t, string(output), expected)
 }
