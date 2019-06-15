b06c609ffb1afd08101143ed33f4394ec7920802
Lukas Gentele <gentele@covexo.com>
2019-06-14 23:55:46 -0700

    enable search

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -10,34 +10,35 @@
 
 const siteConfig = {
   projectName: "devspace-docs",
-  title: 'DevSpace Documentation', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://devspace.cloud', // Your website URL
-  baseUrl: '/',
+  title: "DevSpace Documentation", // Title for your website.
+  tagline: "A website for testing",
+  url: "https://devspace.cloud", // Your website URL
+  baseUrl: "/",
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {href: 'http://devspace.cloud/pricing', label: 'Pricing'},
-    {href: 'http://devspace.cloud/about', label: 'About'},
-    {doc: 'getting-started/installation', label: 'Docs'},
-    {href: 'http://devspace.cloud/help', label: 'Help'},
-    {href: 'https://app.devspace.cloud/', label: 'Login'},
-    {href: '#', label: ''},
+    { href: "http://devspace.cloud/pricing", label: "Pricing" },
+    { href: "http://devspace.cloud/about", label: "About" },
+    { doc: "getting-started/installation", label: "Docs" },
+    { href: "http://devspace.cloud/help", label: "Help" },
+    { href: "https://app.devspace.cloud/", label: "Login" },
+    { href: "#", label: "" },
+    { search: true }
   ],
 
   /* path to images for header/footer */
-  headerIcon: 'img/devspace-logo.svg',
-  footerIcon: 'img/devspace-logo.svg',
-  favicon: 'img/favicon.png',
+  headerIcon: "img/devspace-logo.svg",
+  footerIcon: "img/devspace-logo.svg",
+  favicon: "img/favicon.png",
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/devspace-logo.svg',
-  twitterImage: 'img/devspace-logo.svg',
+  ogImage: "img/devspace-logo.svg",
+  twitterImage: "img/devspace-logo.svg",
 
   /* Colors for website */
   colors: {
-    primaryColor: '#223366',
-    secondaryColor: '#00BDFF',
+    primaryColor: "#223366",
+    secondaryColor: "#00BDFF"
   },
 
   /* Custom fonts for website */
@@ -59,18 +60,18 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'atom-one-dark',
+    theme: "atom-one-dark"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: [
-    'https://buttons.github.io/buttons.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
-    '/js/code-block-buttons.js',
-    '/js/code-block-line-numbers.js',
-    '/js/responsive-menu.js',
-    '/js/tag-manager.js',
-    '/js/onpage-nav.js',
+    "https://buttons.github.io/buttons.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
+    "/js/code-block-buttons.js",
+    "/js/code-block-line-numbers.js",
+    "/js/responsive-menu.js",
+    "/js/tag-manager.js",
+    "/js/onpage-nav.js"
   ],
 
   stylesheets: [
@@ -78,10 +79,10 @@ const siteConfig = {
   ],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
-  disableHeaderTitle:true,
+  disableHeaderTitle: true,
   disableTitleTagline: true,
   // Show documentation's last contributor's name.
   // enableUpdateBy: true,
@@ -89,9 +90,16 @@ const siteConfig = {
   // Show documentation's last update time.
   enableUpdateTime: true,
 
-  customDocsPath: 'pages',
+  customDocsPath: "pages",
   docsSideNavCollapsible: true,
-  editUrl: 'https://github.com/devspace-cloud/devspace/edit/master/docs/pages/',
+  editUrl: "https://github.com/devspace-cloud/devspace/edit/master/docs/pages/",
+
+  algolia: {
+    apiKey: "4339e8c4d6313d53209b996a35e7c0d5",
+    indexName: "devspace",
+    placeholder: "Search...",
+    algoliaOptions: {}
+  }
 };
 
 module.exports = siteConfig;
