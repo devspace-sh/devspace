ffa5295c46e0da3b2e0fcc186fd91862e30a69c5
Lukas Gentele <gentele@covexo.com>
2019-06-16 22:05:28 -0700

    improve docs js for google bot

--- docs/website/static/js/code-block-line-numbers.js
+++ docs/website/static/js/code-block-line-numbers.js
@@ -1,17 +1,26 @@
-window.addEventListener('load', function() {
-    var el = document.getElementsByClassName('hljs')
-    
-    for (let i = 0; i < el.length; i++) {
-        var lineNumber = 0;
-        var lineId;
-        
-        el[i].innerHTML = el[i].innerHTML.replace(/(^|\n)/g, function() {
-            lineNumber++;
-            lineId = 'block-' + i + '-line-' + lineNumber
-            return arguments[1] + '<a class="line" href="#' + lineId + '" id="' + lineId + '">' + lineNumber + '</a>';
-        })
-        var lastLine = document.getElementById(lineId);
+window.addEventListener("load", function() {
+  var el = document.getElementsByClassName("hljs");
 
-        lastLine.parentElement.removeChild(lastLine);
-    }
+  for (let i = 0; i < el.length; i++) {
+    var lineNumber = 0;
+    var lineId;
+
+    el[i].innerHTML = el[i].innerHTML.replace(/(^|\n)/g, function() {
+      lineNumber++;
+      lineId = "block-" + i + "-line-" + lineNumber;
+      return (
+        arguments[1] +
+        '<a class="line" href="#' +
+        lineId +
+        '" id="' +
+        lineId +
+        '">' +
+        lineNumber +
+        "</a>"
+      );
+    });
+    var lastLine = document.getElementById(lineId);
+
+    lastLine.parentElement.removeChild(lastLine);
+  }
 });
