61822d55142e351f7df570de8dfa181cf913adee
Lukas Gentele <gentele@covexo.com>
2019-06-19 00:40:45 -0700

    use devspace react-components for docs

--- docs/website/core/Footer.js
+++ docs/website/core/Footer.js
@@ -5,30 +5,40 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require("react");
+const Chat = require("@devspace/react-components").Chat;
+const Analytics = require("@devspace/react-components").Analytics;
 
 class Footer extends React.Component {
   docUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
     const docsUrl = this.props.config.docsUrl;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
+    const langPart = `${language ? `${language}/` : ""}`;
     return `${baseUrl}${docsPart}${langPart}${doc}`;
   }
 
   pageUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? `${language}/` : '') + doc;
+    return baseUrl + (language ? `${language}/` : "") + doc;
   }
 
   render() {
     return (
       <footer className="nav-footer" id="footer">
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58VC97L" height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
+        <Chat />
+        <Analytics />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-58VC97L"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
       </footer>
     );
   }
 }
-
 
 module.exports = Footer;
