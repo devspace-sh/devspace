8bc0ce4b76a90f80a8660f991930bf5c31c06bf8
Florian <1522784@stud.hs-mannheim.de>
2019-08-07 16:20:32 +0200

    Increase coverage of add cmd to 61,3%

--- cmd/add/add_test.go
+++ cmd/add/add_test.go
@@ -1,10 +1,41 @@
 package add
 
 import (
+	"fmt"
 	"strings"
 	"testing"
+
+	"github.com/devspace-cloud/devspace/pkg/util/log"
+
 	"gotest.tools/assert"
 )
+
+var logOutput string
+
+type testLogger struct {
+	log.DiscardLogger
+}
+
+func (t testLogger) Info(args ...interface{}) {
+	logOutput = logOutput + "\nInfo " + fmt.Sprint(args...)
+}
+func (t testLogger) Infof(format string, args ...interface{}) {
+	logOutput = logOutput + "\nInfo " + fmt.Sprintf(format, args...)
+}
+
+func (t testLogger) Done(args ...interface{}) {
+	logOutput = logOutput + "\nDone " + fmt.Sprint(args...)
+}
+func (t testLogger) Donef(format string, args ...interface{}) {
+	logOutput = logOutput + "\nDone " + fmt.Sprintf(format, args...)
+}
+
+func (t testLogger) Warn(args ...interface{}) {
+	logOutput = logOutput + "\nWarn " + fmt.Sprint(args...)
+}
+func (t testLogger) Warnf(format string, args ...interface{}) {
+	logOutput = logOutput + "\nWarn " + fmt.Sprintf(format, args...)
+}
 
 func TestAdd(t *testing.T) {
 	addCmd := NewAddCmd()
@@ -14,17 +45,17 @@ func TestAdd(t *testing.T) {
 	for _, subcommand := range subcommands {
 		subCommandName := subcommand.Name()
 		index := pos(expectedSubcommandNames, subCommandName)
-		assert.Equal(t, true, index > -1, "Wrong subcommand " + subCommandName)
+		assert.Equal(t, true, index > -1, "Wrong subcommand "+subCommandName)
 		expectedSubcommandNames = append(expectedSubcommandNames[:index], expectedSubcommandNames[index+1:]...)
 	}
-	assert.Equal(t, 0, len(expectedSubcommandNames), "Some subcommands of add are missing: " + strings.Join(expectedSubcommandNames, ", "))
+	assert.Equal(t, 0, len(expectedSubcommandNames), "Some subcommands of add are missing: "+strings.Join(expectedSubcommandNames, ", "))
 }
 
 func pos(slice []string, value string) int {
-    for p, v := range slice {
-        if (v == value) {
-            return p
-        }
-    }
-    return -1
+	for p, v := range slice {
+		if v == value {
+			return p
+		}
+	}
+	return -1
 }
