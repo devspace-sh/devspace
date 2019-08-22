1f0827814d50bb1410a0a391baf93f5c88c58a3d
Florian <1522784@stud.hs-mannheim.de>
2019-08-22 14:09:51 +0200

    Add test for "devspace remove cluster"

--- cmd/remove/cluster_test.go
+++ cmd/remove/cluster_test.go
@@ -1,16 +1,26 @@
 package remove
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"runtime/debug"
+	"strings"
 	"testing"
+	"time"
 
-	"github.com/devspace-cloud/devspace/pkg/devspace/config/configutil"
+	cloudpkg "github.com/devspace-cloud/devspace/pkg/devspace/cloud"
+	cloudconfig "github.com/devspace-cloud/devspace/pkg/devspace/cloud/config"
+	cloudlatest "github.com/devspace-cloud/devspace/pkg/devspace/cloud/config/versions/latest"
+	"github.com/devspace-cloud/devspace/pkg/devspace/cloud/token"
 	"github.com/devspace-cloud/devspace/pkg/devspace/config/constants"
-	"github.com/devspace-cloud/devspace/pkg/devspace/config/versions/latest"
 	"github.com/devspace-cloud/devspace/pkg/util/log"
 	"github.com/devspace-cloud/devspace/pkg/util/survey"
+	homedir "github.com/mitchellh/go-homedir"
 
 	"gotest.tools/assert"
 )
@@ -42,14 +52,42 @@ func (t testLogger) Warnf(format string, args ...interface{}) {
 	logOutput = logOutput + "\nWarn " + fmt.Sprintf(format, args...)
 }
 
+func (t testLogger) StartWait(msg string) {
+	logOutput = logOutput + "\nWait " + fmt.Sprint(msg)
+}
+
+type customGraphqlClient struct {
+	responses []interface{}
+}
+
+func (q *customGraphqlClient) GrapqhlRequest(p *cloudpkg.Provider, request string, vars map[string]interface{}, response interface{}) error {
+	if len(q.responses) == 0 {
+		panic("Not enough responses. Need response for: " + request)
+	}
+	currentResponse := q.responses[0]
+	q.responses = q.responses[1:]
+
+	errorResponse, isError := currentResponse.(error)
+	if isError {
+		return errorResponse
+	}
+	buf, err := json.Marshal(currentResponse)
+	if err != nil {
+		panic(fmt.Sprintf("Cannot encode response. %d responses left", len(q.responses)))
+	}
+	json.NewDecoder(bytes.NewReader(buf)).Decode(&response)
+
+	return nil
+}
+
 type removeClusterTestCase struct {
 	name string
 
-	fakeConfig *latest.Config
-
-	args     []string
-	answers  []string
-	provider string
+	args             []string
+	answers          []string
+	graphQLResponses []interface{}
+	provider         string
+	providerList     []*cloudlatest.Provider
 
 	expectedOutput   string
 	expectedPanic    string
@@ -57,8 +95,93 @@ type removeClusterTestCase struct {
 }
 
 func TestRunRemoveCluster(t *testing.T) {
-	t.Skip("Providerstuff later")
-	testCases := []removeClusterTestCase{}
+	claimAsJSON, _ := json.Marshal(token.ClaimSet{
+		Expiration: time.Now().Add(time.Hour).Unix(),
+	})
+	validEncodedClaim := base64.URLEncoding.EncodeToString(claimAsJSON)
+	for strings.HasSuffix(string(validEncodedClaim), "=") {
+		validEncodedClaim = strings.TrimSuffix(validEncodedClaim, "=")
+	}
+
+	testCases := []removeClusterTestCase{
+		removeClusterTestCase{
+			name:          "Cloud context not gettable",
+			expectedPanic: "Error getting cloud context: Cloud provider not found! Did you run `devspace add provider [url]`? Existing cloud providers: ",
+		},
+		removeClusterTestCase{
+			name:     "Don't delete",
+			provider: "myProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name: "myProvider",
+					Key:  "someKey",
+				},
+			},
+			answers: []string{"no"},
+		},
+		removeClusterTestCase{
+			name:     "Unparsable clustername",
+			provider: "myProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name: "myProvider",
+					Key:  "someKey",
+				},
+			},
+			answers:       []string{"Yes"},
+			args:          []string{"a:b:c"},
+			expectedPanic: "Error parsing cluster name a:b:c: Expected : only once",
+		},
+		removeClusterTestCase{
+			name:     "Cluster can't be deleted",
+			provider: "myProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name:  "myProvider",
+					Key:   "someKey",
+					Token: "." + validEncodedClaim + ".",
+				},
+			},
+			answers: []string{"Yes", "No", "No"},
+			args:    []string{"a:b"},
+			graphQLResponses: []interface{}{
+				struct {
+					Clusters []*cloudpkg.Cluster `json:"cluster"`
+				}{
+					Clusters: []*cloudpkg.Cluster{
+						&cloudpkg.Cluster{},
+					},
+				},
+				fmt.Errorf("Testerror from graphql server"),
+			},
+			expectedPanic:  "Testerror from graphql server",
+			expectedOutput: "\nWait Deleting cluster ",
+		},
+		removeClusterTestCase{
+			name:     "Successful deletion",
+			provider: "myProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name:  "myProvider",
+					Key:   "someKey",
+					Token: "." + validEncodedClaim + ".",
+				},
+			},
+			answers: []string{"Yes", "No", "No"},
+			args:    []string{"a:b"},
+			graphQLResponses: []interface{}{
+				struct {
+					Clusters []*cloudpkg.Cluster `json:"cluster"`
+				}{
+					Clusters: []*cloudpkg.Cluster{
+						&cloudpkg.Cluster{},
+					},
+				},
+				struct{}{},
+			},
+			expectedOutput: "\nWait Deleting cluster \nDone Successfully deleted cluster a:b",
+		},
+	}
 
 	log.SetInstance(&testLogger{
 		log.DiscardLogger{PanicOnExit: true},
@@ -86,14 +209,23 @@ func testRunRemoveCluster(t *testing.T, testCase removeClusterTestCase) {
 		t.Fatalf("Error changing working directory: %v", err)
 	}
 
+	homedir, err := homedir.Dir()
+	assert.NilError(t, err, "Error getting homedir in testCase %s", testCase.name)
+	relDir, err := filepath.Rel(homedir, dir)
+	assert.NilError(t, err, "Error getting relative dir path in testCase %s", testCase.name)
+	cloudconfig.DevSpaceProvidersConfigPath = filepath.Join(relDir, "Doesn'tExist")
+	cloudconfig.LegacyDevSpaceCloudConfigPath = filepath.Join(relDir, "Doesn'tExist")
+
+	providerConfig, err := cloudconfig.ParseProviderConfig()
+	assert.NilError(t, err, "Error getting provider config in testCase %s", testCase.name)
+	providerConfig.Providers = testCase.providerList
+
 	for _, answer := range testCase.answers {
 		survey.SetNextAnswer(answer)
 	}
 
-	isDeploymentsNil := testCase.fakeConfig == nil || testCase.fakeConfig.Deployments == nil
-	configutil.SetFakeConfig(testCase.fakeConfig)
-	if isDeploymentsNil && testCase.fakeConfig != nil {
-		testCase.fakeConfig.Deployments = nil
+	cloudpkg.DefaultGraphqlClient = &customGraphqlClient{
+		responses: testCase.graphQLResponses,
 	}
 
 	defer func() {
@@ -110,18 +242,21 @@ func testRunRemoveCluster(t *testing.T, testCase removeClusterTestCase) {
 		rec := recover()
 		if testCase.expectedPanic == "" {
 			if rec != nil {
-				t.Fatalf("Unexpected panic in testCase %s. Message: %s", testCase.name, rec)
+				t.Fatalf("Unexpected panic in testCase %s. Message: %s. Stack: %s", testCase.name, rec, string(debug.Stack()))
 			}
 		} else {
 			if rec == nil {
 				t.Fatalf("Unexpected no panic in testCase %s", testCase.name)
 			} else {
-				assert.Equal(t, rec, testCase.expectedPanic, "Wrong panic message in testCase %s", testCase.name)
+				assert.Equal(t, rec, testCase.expectedPanic, "Wrong panic message in testCase %s. Stack: %s", testCase.name, string(debug.Stack()))
 			}
 		}
 		assert.Equal(t, logOutput, testCase.expectedOutput, "Unexpected output in testCase %s", testCase.name)
 	}()
 
+	if len(testCase.args) == 0 {
+		testCase.args = []string{""}
+	}
 	(&clusterCmd{
 		Provider: testCase.provider,
 	}).RunRemoveCluster(nil, testCase.args)
