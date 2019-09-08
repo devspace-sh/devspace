49f6506ce25d49410878e8c35b51d044f7214bc7
Lukas Gentele <lg@devspace.cloud>
2019-09-08 15:08:06 -0700

    improve onpage nav

--- docs/website/static/js/onpage-nav.js
+++ docs/website/static/js/onpage-nav.js
@@ -41,43 +41,47 @@ const highlightActiveOnPageLink = function() {
         window.addEventListener('scroll', highlightActiveOnPageLink);
     }
 
-    if (!activeHash) {
-        const anchors = document.querySelectorAll("h2 > .anchor, h3 > .anchor");
-
-        if (document.scrollingElement.scrollTop < 100 && anchors.length > 0) {
-            activeHash = anchors[0].attributes.id.value;
-        } else {
-            for (let i = 0; i < anchors.length; i++) {
-                const anchor = anchors[i];
-
-                if (anchor.parentElement.getBoundingClientRect().top < window.screen.availHeight*0.5) {
-                    activeHash = anchor.attributes.id.value;
+    setTimeout(function() {
+        if (!activeHash) {
+            const anchors = document.querySelectorAll("h2 > .anchor, h3 > .anchor");
+    
+            if (document.scrollingElement.scrollTop < 100 && anchors.length > 0) {
+                activeHash = anchors[0].attributes.id.value;
+            } else {
+                for (let i = 0; i < anchors.length; i++) {
+                    const anchor = anchors[i];
+    
+                    if (anchor.parentElement.getBoundingClientRect().top < window.screen.availHeight*0.7) {
+                        activeHash = anchor.attributes.id.value;
+                    } else {
+                        break;
+                    }
+                }
+            }
+    
+            if (!activeHash) {
+                const firstOnPageNavLink = document.querySelectorAll(".toc-headings:first-child > li:first-child > a");
+    
+                if (firstOnPageNavLink.attributes) {
+                    activeHash = firstOnPageNavLink.attributes.href.value.substr(1);
                 }
             }
         }
     
-        if (!activeHash) {
-            const firstOnPageNavLink = document.querySelectorAll(".toc-headings:first-child > li:first-child > a");
-
-            if (firstOnPageNavLink.attributes) {
-                activeHash = firstOnPageNavLink.attributes.href.value.substr(1);
-            }
+        const allLinks = document.querySelectorAll("a");
+        
+        for (let i = 0; i < allLinks.length; i++) {
+            const link = allLinks[i];
+            link.classList.remove("active");
         }
-    }
-
-    const allLinks = document.querySelectorAll("a");
     
-    for (let i = 0; i < allLinks.length; i++) {
-        const link = allLinks[i];
-        link.classList.remove("active");
-    }
-
-    const activeLinks = document.querySelectorAll("a[href='#" + activeHash + "'");
-
-    for (let i = 0; i < activeLinks.length; i++) {
-        const link = activeLinks[i];
-        link.classList.add("active");
-    }
+        const activeLinks = document.querySelectorAll("a[href='#" + activeHash + "'");
+    
+        for (let i = 0; i < activeLinks.length; i++) {
+            const link = activeLinks[i];
+            link.classList.add("active");
+        }
+    }, 100)
 };
 
 const hashLinkClickSet = false;
