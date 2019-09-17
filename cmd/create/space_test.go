71ee482274d38c5f3f4a5fbc71d5a1448630098d
Florian <1522784@stud.hs-mannheim.de>
2019-09-17 14:16:27 +0200

    Update test for create-cmd

--- cmd/create/space_test.go
+++ cmd/create/space_test.go
@@ -1,20 +1,23 @@
 package create
 
-/* @Florian adjust to new behaviour
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"runtime/debug"
+	"strings"
 	"testing"
+	"time"
 
 	cloudpkg "github.com/devspace-cloud/devspace/pkg/devspace/cloud"
 	cloudconfig "github.com/devspace-cloud/devspace/pkg/devspace/cloud/config"
 	cloudlatest "github.com/devspace-cloud/devspace/pkg/devspace/cloud/config/versions/latest"
+	"github.com/devspace-cloud/devspace/pkg/devspace/cloud/token"
 	"github.com/devspace-cloud/devspace/pkg/util/log"
+	"github.com/devspace-cloud/devspace/pkg/util/survey"
 
 	"gotest.tools/assert"
 )
@@ -56,7 +59,7 @@ type customGraphqlClient struct {
 
 func (q *customGraphqlClient) GrapqhlRequest(p *cloudpkg.Provider, request string, vars map[string]interface{}, response interface{}) error {
 	if len(q.responses) == 0 {
-		panic("Not enough responses. Need response for: " + request)
+		return fmt.Errorf("Custom graphQL server error")
 	}
 	currentResponse := q.responses[0]
 	q.responses = q.responses[1:]
@@ -79,16 +82,26 @@ type createSpaceTestCase struct {
 
 	graphQLResponses []interface{}
 	providerList     []*cloudlatest.Provider
+	args             []string
+	answers          []string
 
 	activeFlag   bool
 	providerFlag string
 	clusterFlag  string
 
 	expectedOutput string
-	expectedPanic  string
+	expectedErr    string
 }
 
 func TestRunCreateSpace(t *testing.T) {
+	claimAsJSON, _ := json.Marshal(token.ClaimSet{
+		Expiration: time.Now().Add(time.Hour).Unix(),
+	})
+	validEncodedClaim := base64.URLEncoding.EncodeToString(claimAsJSON)
+	for strings.HasSuffix(string(validEncodedClaim), "=") {
+		validEncodedClaim = strings.TrimSuffix(validEncodedClaim, "=")
+	}
+
 	testCases := []createSpaceTestCase{
 		createSpaceTestCase{
 			name:         "Provider doesn't Exist",
@@ -98,7 +111,7 @@ func TestRunCreateSpace(t *testing.T) {
 					Name: "SomeProvider",
 				},
 			},
-			expectedPanic: "Cloud provider not found! Did you run `devspace add provider [url]`? Existing cloud providers: SomeProvider ",
+			expectedErr: "Cloud provider not found! Did you run `devspace add provider [url]`? Existing cloud providers: SomeProvider ",
 		},
 		createSpaceTestCase{
 			name:         "Projects can't be retrieved",
@@ -109,11 +122,281 @@ func TestRunCreateSpace(t *testing.T) {
 					Key:  "someKey",
 				},
 			},
-			graphQLResponses: []interface{}{
-				fmt.Errorf("Custom graphQL server error"),
-			},
-			expectedPanic:  "Error retrieving projects: Custom graphQL server error",
+			expectedErr:    "get projects: Custom graphQL server error",
 			expectedOutput: "\nWait Retrieving clusters",
+		},
+		createSpaceTestCase{
+			name:         "New project can't be created",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name: "SomeProvider",
+					Key:  "someKey",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{
+					Projects: []*cloudlatest.Project{},
+				},
+			},
+			expectedErr:    "Custom graphQL server error",
+			expectedOutput: "\nWait Retrieving clusters",
+		},
+		createSpaceTestCase{
+			name:         "Unparsable cluster name",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name: "SomeProvider",
+					Key:  "someKey",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{Projects: []*cloudlatest.Project{&cloudlatest.Project{}}},
+			},
+			clusterFlag:    "a:b:c",
+			expectedErr:    "Error parsing cluster name a:b:c: Expected : only once",
+			expectedOutput: "\nWait Retrieving clusters",
+		},
+		createSpaceTestCase{
+			name:         "Can't get clusters",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name: "SomeProvider",
+					Key:  "someKey",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{Projects: []*cloudlatest.Project{&cloudlatest.Project{}}},
+			},
+			expectedErr:    "get clusters: Custom graphQL server error",
+			expectedOutput: "\nWait Retrieving clusters",
+		},
+		createSpaceTestCase{
+			name:         "No clusters",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name: "SomeProvider",
+					Key:  "someKey",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{Projects: []*cloudlatest.Project{&cloudlatest.Project{}}},
+				struct {
+					Clusters []*cloudlatest.Cluster `json:"cluster"`
+				}{Clusters: []*cloudlatest.Cluster{}},
+			},
+			expectedErr:    "Cannot create space, because no cluster was found",
+			expectedOutput: "\nWait Retrieving clusters",
+		},
+		createSpaceTestCase{
+			name:         "Question 1 is not possible",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name:  "SomeProvider",
+					Key:   "someKey",
+					Token: "." + validEncodedClaim + ".",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{Projects: []*cloudlatest.Project{&cloudlatest.Project{}}},
+				struct {
+					Clusters []*cloudlatest.Cluster `json:"cluster"`
+				}{Clusters: []*cloudlatest.Cluster{
+					&cloudlatest.Cluster{Owner: &cloudlatest.Owner{}},
+					&cloudlatest.Cluster{},
+				}},
+			},
+			expectedErr:    "Cannot ask question 'Which cluster should the space created in?' because logger level is too low",
+			expectedOutput: "\nWait Retrieving clusters",
+		},
+		createSpaceTestCase{
+			name:         "Question 2 is not possible",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name:  "SomeProvider",
+					Key:   "someKey",
+					Token: "." + validEncodedClaim + ".",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{Projects: []*cloudlatest.Project{&cloudlatest.Project{}}},
+				struct {
+					Clusters []*cloudlatest.Cluster `json:"cluster"`
+				}{Clusters: []*cloudlatest.Cluster{
+					&cloudlatest.Cluster{},
+					&cloudlatest.Cluster{},
+				}},
+			},
+			expectedErr:    "Cannot ask question 'Which hosted DevSpace cluster should the space created in?' because logger level is too low",
+			expectedOutput: "\nWait Retrieving clusters",
+		},
+		createSpaceTestCase{
+			name:         "Select non existent cluster",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name:  "SomeProvider",
+					Key:   "someKey",
+					Token: "." + validEncodedClaim + ".",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{Projects: []*cloudlatest.Project{&cloudlatest.Project{}}},
+				struct {
+					Clusters []*cloudlatest.Cluster `json:"cluster"`
+				}{Clusters: []*cloudlatest.Cluster{
+					&cloudlatest.Cluster{},
+					&cloudlatest.Cluster{},
+				}},
+			},
+			answers: []string{"notthere"},
+			expectedErr:    "No cluster selected",
+			expectedOutput: "\nWait Retrieving clusters",
+		},
+		createSpaceTestCase{
+			name:         "Space creation fails with server error",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name:  "SomeProvider",
+					Key:   "someKey",
+					Token: "." + validEncodedClaim + ".",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{Projects: []*cloudlatest.Project{&cloudlatest.Project{}}},
+				struct {
+					Clusters []*cloudlatest.Cluster `json:"cluster"`
+				}{Clusters: []*cloudlatest.Cluster{
+					&cloudlatest.Cluster{Owner: &cloudlatest.Owner{}},
+				}},
+			},
+			expectedErr:    "create space: Custom graphQL server error",
+			expectedOutput: "\nWait Retrieving clusters\nWait Creating space ",
+		},
+		createSpaceTestCase{
+			name:         "Get space fails after creation",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name:  "SomeProvider",
+					Key:   "someKey",
+					Token: "." + validEncodedClaim + ".",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{Projects: []*cloudlatest.Project{&cloudlatest.Project{}}},
+				struct {
+					Clusters []*cloudlatest.Cluster `json:"cluster"`
+				}{Clusters: []*cloudlatest.Cluster{
+					&cloudlatest.Cluster{Owner: &cloudlatest.Owner{}, Name: "cluster1"},
+					&cloudlatest.Cluster{Owner: &cloudlatest.Owner{}, Name: "cluster2"},
+				}},
+				struct {
+					CreateSpace interface{} `json:"manager_createSpace"`
+				}{CreateSpace: struct{ SpaceID int }{SpaceID: 1}},
+			},
+			answers:        []string{"cluster1"},
+			expectedErr:    "get space: Custom graphQL server error",
+			expectedOutput: "\nWait Retrieving clusters\nWait Creating space ",
+		},
+		createSpaceTestCase{
+			name:         "Get serviceaccount fails after creation",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name:  "SomeProvider",
+					Key:   "someKey",
+					Token: "." + validEncodedClaim + ".",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{[]*cloudlatest.Project{&cloudlatest.Project{}}},
+				struct {
+					Clusters []*cloudlatest.Cluster `json:"cluster"`
+				}{[]*cloudlatest.Cluster{
+					&cloudlatest.Cluster{Owner: &cloudlatest.Owner{}, Name: "cluster1"},
+					&cloudlatest.Cluster{Name: "cluster2"},
+				}},
+				struct {
+					CreateSpace interface{} `json:"manager_createSpace"`
+				}{CreateSpace: struct{ SpaceID int }{SpaceID: 1}},
+				struct {
+					Space interface{} `json:"space_by_pk"`
+				}{
+					struct {
+						KubeContext interface{} `json:"kube_context"`
+						Owner       interface{} `json:"account"`
+					}{struct{Cluster interface{} `json:"cluster"`}{struct{}{}}, struct{}{}},
+				},
+			},
+			answers:        []string{DevSpaceCloudHostedCluster},
+			expectedErr:    "get serviceaccount: Custom graphQL server error",
+			expectedOutput: "\nWait Retrieving clusters\nWait Creating space ",
+		},
+		createSpaceTestCase{
+			name:         "Undecodable CACert",
+			providerFlag: "SomeProvider",
+			providerList: []*cloudlatest.Provider{
+				&cloudlatest.Provider{
+					Name:  "SomeProvider",
+					Key:   "someKey",
+					Token: "." + validEncodedClaim + ".",
+				},
+			},
+			graphQLResponses: []interface{}{
+				struct {
+					Projects []*cloudlatest.Project `json:"project"`
+				}{[]*cloudlatest.Project{&cloudlatest.Project{}}},
+				struct {
+					Clusters []*cloudlatest.Cluster `json:"cluster"`
+				}{[]*cloudlatest.Cluster{
+					&cloudlatest.Cluster{Name: "cluster2"},
+					&cloudlatest.Cluster{Name: "cluster3"},
+				}},
+				struct {
+					CreateSpace interface{} `json:"manager_createSpace"`
+				}{CreateSpace: struct{ SpaceID int }{SpaceID: 1}},
+				struct {
+					Space interface{} `json:"space_by_pk"`
+				}{
+					struct {
+						KubeContext interface{} `json:"kube_context"`
+						Owner       interface{} `json:"account"`
+					}{struct{Cluster interface{} `json:"cluster"`}{struct{}{}}, struct{}{}},
+				},
+				struct {ServiceAccount *cloudlatest.ServiceAccount `json:"manager_serviceAccount"`}{&cloudlatest.ServiceAccount{
+					CaCert: "undecodable",
+				}},
+			},
+			answers:        []string{"cluster2"},
+			expectedErr:    "update kube context: illegal base64 data at input byte 8",
+			expectedOutput: "\nWait Retrieving clusters\nWait Creating space ",
 		},
 	}
 
@@ -158,18 +441,6 @@ func testRunCreateSpace(t *testing.T, testCase createSpaceTestCase) {
 			t.Fatalf("Error removing dir: %v", err)
 		}
 
-		rec := recover()
-		if testCase.expectedPanic == "" {
-			if rec != nil {
-				t.Fatalf("Unexpected panic in testCase %s. Message: %s. Stack: %s", testCase.name, rec, string(debug.Stack()))
-			}
-		} else {
-			if rec == nil {
-				t.Fatalf("Unexpected no panic in testCase %s", testCase.name)
-			} else {
-				assert.Equal(t, rec, testCase.expectedPanic, "Wrong panic message in testCase %s. Stack: %s", testCase.name, string(debug.Stack()))
-			}
-		}
 		assert.Equal(t, logOutput, testCase.expectedOutput, "Unexpected output in testCase %s", testCase.name)
 	}()
 
@@ -181,10 +452,22 @@ func testRunCreateSpace(t *testing.T, testCase createSpaceTestCase) {
 	assert.NilError(t, err, "Error getting provider config in testCase %s", testCase.name)
 	providerConfig.Providers = testCase.providerList
 
-	(&spaceCmd{
+	for _, answer := range testCase.answers {
+		survey.SetNextAnswer(answer)
+	}
+
+	if len(testCase.args) == 0 {
+		testCase.args = []string{""}
+	}
+	err = (&spaceCmd{
 		Active:   testCase.activeFlag,
 		Provider: testCase.providerFlag,
 		Cluster:  testCase.clusterFlag,
-	}).RunCreateSpace(nil, nil)
+	}).RunCreateSpace(nil, testCase.args)
+
+	if testCase.expectedErr == "" {
+		assert.NilError(t, err, "Unexpected error in testCase %s.", testCase.name)
+	} else {
+		assert.Error(t, err, testCase.expectedErr, "Wrong or no error in testCase %s.", testCase.name)
+	}
 }
-*/
