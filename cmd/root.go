07ab9abd4107309114991b1d02d81b4cbcb3d8e5
fabiankramm <kramm@covexo.com>
2019-10-30 14:45:22 +0100

    Refactor analytics

--- cmd/root.go
+++ cmd/root.go
@@ -60,10 +60,15 @@ func Execute() {
 	// Report any panics
 	defer cloudanalytics.ReportPanics()
 
+	go func() {
+		cloudanalytics.SendCommandEvent(nil)
+	}()
+
 	// Execute command
 	err := rootCmd.Execute()
-	cloudanalytics.SendCommandEvent(err)
 	if err != nil {
+		cloudanalytics.SendCommandEvent(err)
+
 		// Check if return code error
 		retCode, ok := err.(*exit.ReturnCodeError)
 		if ok {
