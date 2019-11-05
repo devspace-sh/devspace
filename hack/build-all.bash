839610717d36ebcb2aa9b5d227cd4e3735a651d5
fabiankramm <kramm@covexo.com>
2019-11-05 15:47:24 +0100

    add deploy job

--- hack/build-all.bash
+++ hack/build-all.bash
@@ -17,7 +17,6 @@ VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 COMMIT_HASH=$(git rev-parse --short HEAD 2>/dev/null)
 DATE=$(date "+%Y-%m-%d")
 BUILD_PLATFORM=$(uname -a | awk '{print tolower($1);}')
-IMPORT_DURING_SOLVE=${IMPORT_DURING_SOLVE:-false}
 
 if [[ "$(pwd)" != "${DEVSPACE_ROOT}" ]]; then
   echo "you are not in the root of the repo" 1>&2
@@ -25,8 +24,8 @@ if [[ "$(pwd)" != "${DEVSPACE_ROOT}" ]]; then
   exit 1
 fi
 
-GO_BUILD_CMD="go build -a -installsuffix cgo"
-GO_BUILD_LDFLAGS="-s -w -X main.commitHash=${COMMIT_HASH} -X main.buildDate=${DATE} -X main.version=${VERSION} -X github.com/devspace-cloud/devspace/pkg/util/analytics.token=${ANALYTICS_TOKEN} -X github.com/devspace-cloud/devspace/pkg/util/analytics.eventEndpoint=${ANALYTICS_ENDPOINT_EVENT} -X github.com/devspace-cloud/devspace/pkg/util/analytics.userEndpoint=${ANALYTICS_ENDPOINT_USER} -X main.flagImportDuringSolve=${IMPORT_DURING_SOLVE}"
+GO_BUILD_CMD="go build -a"
+GO_BUILD_LDFLAGS="-s -w -X main.commitHash=${COMMIT_HASH} -X main.buildDate=${DATE} -X main.version=${VERSION} -X github.com/devspace-cloud/devspace/pkg/util/analytics.token=${ANALYTICS_TOKEN} -X github.com/devspace-cloud/devspace/pkg/util/analytics.eventEndpoint=${ANALYTICS_ENDPOINT_EVENT} -X github.com/devspace-cloud/devspace/pkg/util/analytics.userEndpoint=${ANALYTICS_ENDPOINT_USER}"
 
 if [[ -z "${DEVSPACE_BUILD_PLATFORMS}" ]]; then
     DEVSPACE_BUILD_PLATFORMS="linux windows darwin"
@@ -45,20 +44,12 @@ for OS in ${DEVSPACE_BUILD_PLATFORMS[@]}; do
       NAME="${NAME}.exe"
     fi
 
-    # Enable CGO if building for OS X on OS X; see
-    # https://github.com/golang/dep/issues/1838 for details.
-    if [[ "${OS}" == "darwin" && "${BUILD_PLATFORM}" == "darwin" ]]; then
-      CGO_ENABLED=1
-    else
-      CGO_ENABLED=0
-    fi
-
     if [[ "${ARCH}" == "ppc64" || "${ARCH}" == "ppc64le" ]] && [[ "${OS}" != "linux" ]]; then
         # ppc64 and ppc64le are only supported on Linux.
         echo "Building for ${OS}/${ARCH} not supported."
     else
-        echo "Building for ${OS}/${ARCH} with CGO_ENABLED=${CGO_ENABLED}"
-        GOARCH=${ARCH} GOOS=${OS} CGO_ENABLED=${CGO_ENABLED} ${GO_BUILD_CMD} -ldflags "${GO_BUILD_LDFLAGS}"\
+        echo "Building for ${OS}/${ARCH}"
+        GOARCH=${ARCH} GOOS=${OS} ${GO_BUILD_CMD} -ldflags "${GO_BUILD_LDFLAGS}"\
             -o "${DEVSPACE_ROOT}/release/${NAME}" .
         shasum -a 256 "${DEVSPACE_ROOT}/release/${NAME}" > "${DEVSPACE_ROOT}/release/${NAME}".sha256
     fi
