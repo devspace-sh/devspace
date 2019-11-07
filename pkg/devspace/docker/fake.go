201aeae64e0995c0be181d1a0377ac61c611afd7
fabiankramm <kramm@covexo.com>
2019-11-07 11:45:27 +0100

    Improve docker fake client

--- pkg/devspace/docker/fake.go
+++ pkg/devspace/docker/fake.go
@@ -1,6 +1,7 @@
 package docker
 
 import (
+	"bytes"
 	"context"
 	"io"
 	"strings"
@@ -8,31 +9,26 @@ import (
 	"github.com/devspace-cloud/devspace/pkg/util/log"
 	dockertypes "github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
-	"github.com/pkg/errors"
 )
 
 var fakeClient ClientInterface
 
-//SetFakeClient causes NewClient to return the given fake client. !ONLY FOR TESTING!
+// SetFakeClient causes NewClient to return the given fake client. !ONLY FOR TESTING!
 func SetFakeClient(fake ClientInterface) {
 	fakeClient = fake
 }
 
-//FakeClient is a prototype for a fake docker cient for testing purposes
+// FakeClient is a prototype for a fake docker cient for testing purposes
 type FakeClient struct {
-	PingVal              dockertypes.Ping
-	ImageBuildResponses  []dockertypes.ImageBuildResponse
-	ImagePushResponses   []io.ReadCloser
-	DeleteImageResponses [][]dockertypes.ImageDeleteResponseItem
-	AuthConfig           *dockertypes.AuthConfig
+	AuthConfig *dockertypes.AuthConfig
 }
 
-//Ping is a fake implementation
+// Ping is a fake implementation
 func (client *FakeClient) Ping(ctx context.Context) (dockertypes.Ping, error) {
-	return client.PingVal, nil
+	return dockertypes.Ping{}, nil
 }
 
-//NegotiateAPIVersion is a fake implementation
+// NegotiateAPIVersion is a fake implementation
 func (client *FakeClient) NegotiateAPIVersion(ctx context.Context) {}
 
 // ImageBuildCLI builds an image with the docker cli
@@ -40,41 +36,40 @@ func (client *FakeClient) ImageBuildCLI(useBuildkit bool, context io.Reader, wri
 	return nil
 }
 
-//ImageBuild is a fake implementation
+type nopCloser struct {
+	io.Reader
+}
+
+func (nopCloser) Close() error { return nil }
+
+// ImageBuild is a fake implementation
 func (client *FakeClient) ImageBuild(ctx context.Context, context io.Reader, options dockertypes.ImageBuildOptions) (dockertypes.ImageBuildResponse, error) {
-	response := client.ImageBuildResponses[0]
-	client.ImageBuildResponses = client.ImageBuildResponses[1:]
-	return response, nil
+	return dockertypes.ImageBuildResponse{
+		Body: nopCloser{bytes.NewBufferString("")},
+	}, nil
 }
 
-//ImagePush is a fake implementation
+// ImagePush is a fake implementation
 func (client *FakeClient) ImagePush(ctx context.Context, ref string, options dockertypes.ImagePushOptions) (io.ReadCloser, error) {
-	response := client.ImagePushResponses[0]
-	client.ImagePushResponses = client.ImagePushResponses[1:]
-	return response, nil
+	return nopCloser{bytes.NewBufferString("")}, nil
 }
 
-//Login is a fake implementation
+// Login is a fake implementation
 func (client *FakeClient) Login(registryURL, user, password string, checkCredentialsStore, saveAuthConfig, relogin bool) (*dockertypes.AuthConfig, error) {
-	if user == client.AuthConfig.Username && password == client.AuthConfig.Password {
-		return client.AuthConfig, nil
-	}
-	return nil, errors.New("Wrong username or password")
+	return client.AuthConfig, nil
 }
 
-//DeleteImageByName is a fake implementation
+// DeleteImageByName is a fake implementation
 func (client *FakeClient) DeleteImageByName(imageName string, log log.Logger) ([]dockertypes.ImageDeleteResponseItem, error) {
 	return client.DeleteImageByFilter(filters.NewArgs(filters.Arg("reference", strings.TrimSpace(imageName))), log)
 }
 
-//DeleteImageByFilter is a fake implementation
+// DeleteImageByFilter is a fake implementation
 func (client *FakeClient) DeleteImageByFilter(filter filters.Args, log log.Logger) ([]dockertypes.ImageDeleteResponseItem, error) {
-	response := client.DeleteImageResponses[0]
-	client.DeleteImageResponses = client.DeleteImageResponses[1:]
-	return response, nil
+	return []dockertypes.ImageDeleteResponseItem{}, nil
 }
 
-//GetAuthConfig is a fake implementation
+// GetAuthConfig is a fake implementation
 func (client *FakeClient) GetAuthConfig(registryURL string, checkCredentialsStore bool) (*dockertypes.AuthConfig, error) {
 	return client.AuthConfig, nil
 }
