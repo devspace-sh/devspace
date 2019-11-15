5bd3be0745f5307e79ef2e86c2329e38dcf10649
fabiankramm <kramm@covexo.com>
2019-11-15 11:18:44 +0100

    Test

--- examples/quickstart/index.js
+++ examples/quickstart/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World DevSpace!');
+  res.send('Hello World!');
 });
 
 app.listen(3000, function () {
