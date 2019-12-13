35b087179a54b319ae9005d30952d10a243c65f1
Florian <1522784@stud.hs-mannheim.de>
2019-12-13 11:39:02 +0100

    Remove space from provider.yaml when deleted

--- pkg/devspace/cloud/delete.go
+++ pkg/devspace/cloud/delete.go
@@ -3,9 +3,10 @@ package cloud
 import (
 	"github.com/devspace-cloud/devspace/pkg/devspace/cloud/config/versions/latest"
 	"github.com/devspace-cloud/devspace/pkg/util/kubeconfig"
+	"github.com/pkg/errors"
 )
 
-// DeleteKubeContext removes the specified devspace id from the kube context if it exists
+// DeleteKubeContext removes the specified space from the kube context and providers.yaml
 func (p *provider) DeleteKubeContext(space *latest.Space) error {
 	kubeContext := GetKubeContextNameFromSpace(space.Name, space.ProviderName)
 	kubeConfig, err := kubeconfig.LoadRawConfig()
@@ -18,5 +19,28 @@ func (p *provider) DeleteKubeContext(space *latest.Space) error {
 		return err
 	}
 
-	return kubeconfig.SaveConfig(kubeConfig)
+	err = kubeconfig.SaveConfig(kubeConfig)
+	if err != nil {
+		return errors.Wrap(err, "save kube config")
+	}
+
+	providerConfig, err := p.loader.Load()
+	if err != nil {
+		return errors.Wrap(err, "load provider config")
+	}
+
+	for _, profile := range providerConfig.Providers {
+		for id := range profile.Spaces {
+			if id == space.SpaceID {
+				delete(profile.Spaces, id)
+			}
+		}
+	}
+
+	err = p.loader.Save(providerConfig)
+	if err != nil {
+		return errors.Wrap(err, "save provider config")
+	}
+
+	return nil
 }
