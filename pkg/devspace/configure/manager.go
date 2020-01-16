8e9958c543f805ab37ff74510d0fec455998ad5a
Florian <florian.kutz@t-online.de>
2020-01-16 12:03:28 +0100

    Refactor configure-package

---
+++ pkg/devspace/configure/manager.go
@@ -0,0 +1,43 @@
+package configure
+
+import (
+	cloudconfig "github.com/devspace-cloud/devspace/pkg/devspace/cloud/config"
+	"github.com/devspace-cloud/devspace/pkg/devspace/config/generated"
+	"github.com/devspace-cloud/devspace/pkg/devspace/config/versions/latest"
+	"github.com/devspace-cloud/devspace/pkg/devspace/docker"
+	"github.com/devspace-cloud/devspace/pkg/util/log"
+)
+
+// Manager controls the devspace configuration
+type Manager interface {
+	NewDockerfileComponentDeployment(generatedConfig *generated.Config, name, imageName, dockerfile, context string) (*latest.ImageConfig, *latest.DeploymentConfig, error)
+	NewImageComponentDeployment(name, imageName string) (*latest.ImageConfig, *latest.DeploymentConfig, error)
+	NewPredefinedComponentDeployment(name, component string) (*latest.DeploymentConfig, error)
+	NewKubectlDeployment(name, manifests string) (*latest.DeploymentConfig, error)
+	NewHelmDeployment(name, chartName, chartRepo, chartVersion string) (*latest.DeploymentConfig, error)
+	RemoveDeployment(removeAll bool, name string) (bool, error)
+
+	AddImage(nameInConfig, name, tag, contextPath, dockerfilePath, buildTool string) error
+	RemoveImage(removeAll bool, names []string) error
+
+	AddPort(namespace, labelSelector string, args []string) error
+	RemovePort(removeAll bool, labelSelector string, args []string) error
+
+	AddSyncPath(localPath, containerPath, namespace, labelSelector, excludedPathsString string) error
+	RemoveSyncPath(removeAll bool, localPath, containerPath, labelSelector string) error
+}
+
+type manager struct {
+	log               log.Logger
+	config            *latest.Config
+	cloudConfigLoader cloudconfig.Loader
+	dockerClient      docker.Client
+}
+
+// NewManager creates a new instance of the interface Manager
+func NewManager(config *latest.Config, log log.Logger) Manager {
+	return &manager{
+		log:    log,
+		config: config,
+	}
+}
