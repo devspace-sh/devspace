4864b279000779b31296c816578c81cd212b662e
1522784 <1522784@stud.hs-mannheim.de>
2020-03-26 12:14:03 +0100

    Test kubectl package with 53%

--- pkg/devspace/kubectl/client_test.go
+++ pkg/devspace/kubectl/client_test.go
@@ -7,21 +7,28 @@ import (
 	"github.com/devspace-cloud/devspace/pkg/devspace/config/generated"
 	fakekubeloader "github.com/devspace-cloud/devspace/pkg/util/kubeconfig/testing"
 	log "github.com/devspace-cloud/devspace/pkg/util/log/testing"
+	fakesurvey "github.com/devspace-cloud/devspace/pkg/util/survey/testing"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 	"gotest.tools/assert"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
 type newClientFromContextTestCase struct {
 	name string
 
-	context       string
-	namespace     string
-	switchContext bool
-	contexts      map[string]*clientcmdapi.Context
+	clusters       map[string]*clientcmdapi.Cluster
+	context        string
+	namespace      string
+	switchContext  bool
+	contexts       map[string]*clientcmdapi.Context
+	currentContext string
 
 	expectedErr    bool
 	expectedClient *client
@@ -30,19 +37,59 @@ type newClientFromContextTestCase struct {
 func TestNewClientFromContext(t *testing.T) {
 	testCases := []newClientFromContextTestCase{
 		{
-			name:        "context not there",
-			context:     "notThere",
+			name:        "no clusters",
 			expectedErr: true,
+		},
+		{
+			name:    "context not there",
+			context: "notThere",
+			clusters: map[string]*clientcmdapi.Cluster{
+				"": {},
+			},
+			currentContext: "current",
+			expectedErr:    true,
+		},
+		{
+			name:    "Create successfully",
+			context: "context1",
+			clusters: map[string]*clientcmdapi.Cluster{
+				"": {
+					Server: "someServer",
+				},
+			},
+			contexts: map[string]*clientcmdapi.Context{
+				"context1": {
+					Namespace: "contextNamespace",
+				},
+			},
+			switchContext:  true,
+			currentContext: "current",
+			namespace:      "paramNS",
+			expectedClient: &client{
+				Client: &kubernetes.Clientset{
+					DiscoveryClient: &discovery.DiscoveryClient{
+						LegacyPrefix: "/api",
+					},
+				},
+				ClientConfig: &clientcmd.DirectClientConfig{},
+				restConfig: &rest.Config{
+					Host: "someServer",
+				},
+				currentContext: "context1",
+				namespace:      "paramNS",
+			},
 		},
 	}
 
 	for _, testCase := range testCases {
 		kubeLoader := &fakekubeloader.Loader{
 			RawConfig: &clientcmdapi.Config{
-				Contexts: testCase.contexts,
+				Clusters:       testCase.clusters,
+				Contexts:       testCase.contexts,
+				CurrentContext: testCase.currentContext,
 			},
 		}
-		client, err := NewClientFromContext(testCase.context, testCase.namespace, testCase.switchContext, kubeLoader)
+		result, err := NewClientFromContext(testCase.context, testCase.namespace, testCase.switchContext, kubeLoader)
 
 		if !testCase.expectedErr {
 			assert.NilError(t, err, "Error in testCase %s", testCase.name)
@@ -50,11 +97,114 @@ func TestNewClientFromContext(t *testing.T) {
 			t.Fatalf("Unexpected no error in testCase %s", testCase.name)
 		}
 
-		clientAsYaml, err := yaml.Marshal(client)
+		clientAsYaml, err := yaml.Marshal(result)
 		assert.NilError(t, err, "Error parsing client to yaml in testCase %s", testCase.name)
 		expectedAsYaml, err := yaml.Marshal(testCase.expectedClient)
 		assert.NilError(t, err, "Error parsing expection to yaml in testCase %s", testCase.name)
 		assert.Equal(t, string(clientAsYaml), string(expectedAsYaml), "Unexpected client in testCase %s", testCase.name)
+		if testCase.expectedClient != nil {
+			cl := result.(*client)
+			assert.Equal(t, cl.restConfig.ServerName, testCase.expectedClient.restConfig.ServerName, "Unexpected client server in testCase %s", testCase.name)
+			assert.Equal(t, cl.currentContext, testCase.expectedClient.currentContext, "Unexpected client in testCase %s", testCase.name)
+			assert.Equal(t, cl.namespace, testCase.expectedClient.namespace, "Unexpected client in testCase %s", testCase.name)
+		}
+	}
+}
+
+type newClientBySelectTestCase struct {
+	name string
+
+	allowPrivate   bool
+	switchContext  bool
+	answers        []string
+	clusters       map[string]*clientcmdapi.Cluster
+	context        string
+	namespace      string
+	contexts       map[string]*clientcmdapi.Context
+	currentContext string
+
+	expectedErr    bool
+	expectedClient *client
+}
+
+func TestNewClientBySelect(t *testing.T) {
+	testCases := []newClientBySelectTestCase{
+		{
+			name:        "no contexts",
+			expectedErr: true,
+		},
+		{
+			name:    "Create successfully",
+			context: "context1",
+			clusters: map[string]*clientcmdapi.Cluster{
+				"cluster1": {
+					Server: "someServer",
+				},
+				"private": {
+					Server: "http://127.0.0.1:80/path",
+				},
+			},
+			contexts: map[string]*clientcmdapi.Context{
+				"context1": {
+					Cluster:   "cluster1",
+					Namespace: "contextNamespace",
+				},
+				"private": {
+					Cluster: "private",
+				},
+			},
+			currentContext: "current",
+			switchContext:  true,
+			expectedClient: &client{
+				Client: &kubernetes.Clientset{
+					DiscoveryClient: &discovery.DiscoveryClient{
+						LegacyPrefix: "/api",
+					},
+				},
+				ClientConfig: &clientcmd.DirectClientConfig{},
+				restConfig: &rest.Config{
+					Host: "someServer",
+				},
+				currentContext: "context1",
+				namespace:      "contextNamespace",
+			},
+			answers: []string{"private", "context1"},
+		},
+	}
+
+	for _, testCase := range testCases {
+		kubeLoader := &fakekubeloader.Loader{
+			RawConfig: &clientcmdapi.Config{
+				Clusters:       testCase.clusters,
+				Contexts:       testCase.contexts,
+				CurrentContext: testCase.currentContext,
+			},
+		}
+		logger := &log.FakeLogger{
+			Survey: fakesurvey.NewFakeSurvey(),
+		}
+		for _, answer := range testCase.answers {
+			logger.Survey.SetNextAnswer(answer)
+		}
+		result, err := NewClientBySelect(testCase.allowPrivate, testCase.switchContext, kubeLoader, logger)
+
+		if !testCase.expectedErr {
+			assert.NilError(t, err, "Error in testCase %s", testCase.name)
+		} else if err == nil {
+			t.Fatalf("Unexpected no error in testCase %s", testCase.name)
+		}
+
+		clientAsYaml, err := yaml.Marshal(result)
+		assert.NilError(t, err, "Error parsing client to yaml in testCase %s", testCase.name)
+		expectedAsYaml, err := yaml.Marshal(testCase.expectedClient)
+		assert.NilError(t, err, "Error parsing expection to yaml in testCase %s", testCase.name)
+		assert.Equal(t, string(clientAsYaml), string(expectedAsYaml), "Unexpected client in testCase %s", testCase.name)
+		if testCase.expectedClient != nil {
+			cl := result.(*client)
+			assert.Equal(t, cl.restConfig.ServerName, testCase.expectedClient.restConfig.ServerName, "Unexpected client server in testCase %s", testCase.name)
+			assert.Equal(t, cl.currentContext, testCase.expectedClient.currentContext, "Unexpected client in testCase %s", testCase.name)
+			assert.Equal(t, cl.namespace, testCase.expectedClient.namespace, "Unexpected client in testCase %s", testCase.name)
+		}
 	}
 }
 
