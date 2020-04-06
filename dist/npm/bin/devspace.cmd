117f7cd72ef0e08a0d45f0da08aa429ccbb9ba88
Lukas Gentele <lg@devspace.cloud>
2020-04-06 04:14:55 +0200

    install: improve npm installer

--- dist/npm/bin/devspace.cmd
+++ dist/npm/bin/devspace.cmd
@@ -1,4 +1,11 @@
-echo "Finishing installation for DevSpace CLI"
+@echo off
 
-node ..\index.js force-install && devspace %*
+WHERE devspace >nul 2>nul
+IF %ERRORLEVEL% EQU 0 devspace %* && exit /b %errorlevel%
+
+echo Finishing installation for DevSpace CLI
+
+for %%F in (%0) do set dirname=%%~dpF
+
+node %dirname%\..\index.js force-install && devspace %*
 exit /b %errorlevel%
