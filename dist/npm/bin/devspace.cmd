36478f21eae40cde0780e8dae602af1a2087956e
Lukas Gentele <lg@devspace.cloud>
2020-04-08 18:19:07 +0200

    installer: improve output

--- dist/npm/bin/devspace.cmd
+++ dist/npm/bin/devspace.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-WHERE devspace >nul 2>nul
+WHERE devspace.exe >nul 2>nul
 IF %ERRORLEVEL% EQU 0 devspace %* && exit /b %errorlevel%
 
 echo Finishing installation for DevSpace CLI
@@ -9,4 +9,5 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`npm root -g`) DO (
 SET basedir=%%F
 )
 
+echo "Running: node %basedir%\devspace\index.js force-install"
 node %basedir%\devspace\index.js force-install && devspace.exe %*
