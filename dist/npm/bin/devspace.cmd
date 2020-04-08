2b1bdd4ea5363d159cd6f10407d7073daf521df9
Lukas Gentele <lg@devspace.cloud>
2020-04-09 00:11:08 +0200

    ci: test npm installer

--- dist/npm/bin/devspace.cmd
+++ dist/npm/bin/devspace.cmd
@@ -3,7 +3,7 @@
 WHERE devspace.exe >nul 2>nul
 IF %ERRORLEVEL% EQU 0 devspace %* && exit /b %errorlevel%
 
-echo Finishing installation for DevSpace CLI
+echo "Finishing installation of DevSpace CLI"
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`npm root -g`) DO (
 SET basedir=%%F
