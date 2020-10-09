8db5d944d939479cf9658df8a1e8ea5630f5da0f
fabiankramm <kramm@covexo.com>
2020-10-09 11:49:34 +0200

    fix(ui): resize terminal correctly in the backend

--- pkg/devspace/server/enter.go
+++ pkg/devspace/server/enter.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"github.com/devspace-cloud/devspace/pkg/devspace/kubectl"
+	"github.com/pkg/errors"
+	"k8s.io/client-go/tools/remotecommand"
 	"net/http"
+	"strconv"
 	"time"
 
 	v1 "k8s.io/api/core/v1"
@@ -37,6 +40,22 @@ func (h *handler) enter(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var terminalResizeQueue TerminalResizeQueue
+	resizeId, ok := r.URL.Query()["resize_id"]
+	if ok && len(resizeId) == 1 {
+		h.terminalResizeQueuesMutex.Lock()
+		terminalResizeQueue = newTerminalSizeQueue()
+		h.terminalResizeQueues[resizeId[0]] = terminalResizeQueue
+		h.terminalResizeQueuesMutex.Unlock()
+
+		defer func() {
+			h.terminalResizeQueuesMutex.Lock()
+			defer h.terminalResizeQueuesMutex.Unlock()
+
+			delete(h.terminalResizeQueues, resizeId[0])
+		}()
+	}
+
 	// Create kubectl client
 	client, err := h.getClientFromCache(kubeContext, kubeNamespace)
 	if err != nil {
@@ -63,13 +82,14 @@ func (h *handler) enter(w http.ResponseWriter, r *http.Request) {
 				Namespace: namespace[0],
 			},
 		},
-		Container: container[0],
-		Command:   []string{"sh", "-c", "command -v bash >/dev/null 2>&1 && exec bash || exec sh"},
-		ForceTTY:  true,
-		TTY:       true,
-		Stdin:     stream,
-		Stdout:    stream,
-		Stderr:    stream,
+		Container:         container[0],
+		Command:           []string{"sh", "-c", "command -v bash >/dev/null 2>&1 && exec bash || exec sh"},
+		ForceTTY:          true,
+		TTY:               true,
+		TerminalSizeQueue: terminalResizeQueue,
+		Stdin:             stream,
+		Stdout:            stream,
+		Stderr:            stream,
 	})
 	if err != nil {
 		h.log.Errorf("Error in %s: %v", r.URL.String(), err)
@@ -79,4 +99,80 @@ func (h *handler) enter(w http.ResponseWriter, r *http.Request) {
 
 	ws.SetWriteDeadline(time.Now().Add(time.Second * 5))
 	ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+}
+
+func (h *handler) resize(w http.ResponseWriter, r *http.Request) {
+	resizeId, ok := r.URL.Query()["resize_id"]
+	if !ok || len(resizeId) != 1 {
+		http.Error(w, "resize_id is missing", http.StatusBadRequest)
+		return
+	}
+	widthStr, ok := r.URL.Query()["width"]
+	if !ok || len(widthStr) != 1 {
+		http.Error(w, "width is missing", http.StatusBadRequest)
+		return
+	}
+	heightStr, ok := r.URL.Query()["height"]
+	if !ok || len(heightStr) != 1 {
+		http.Error(w, "height is missing", http.StatusBadRequest)
+		return
+	}
+	width, err := strconv.Atoi(widthStr[0])
+	if err != nil {
+		http.Error(w, errors.Wrap(err, "parse width").Error(), http.StatusBadRequest)
+		return
+	}
+	height, err := strconv.Atoi(heightStr[0])
+	if err != nil {
+		http.Error(w, errors.Wrap(err, "parse height").Error(), http.StatusBadRequest)
+		return
+	}
+
+	h.terminalResizeQueuesMutex.Lock()
+	defer h.terminalResizeQueuesMutex.Unlock()
+
+	resizeQueue, ok := h.terminalResizeQueues[resizeId[0]]
+	if !ok {
+		http.Error(w, "resize_id does not exist", http.StatusBadRequest)
+		return
+	}
+
+	resizeQueue.Resize(remotecommand.TerminalSize{
+		Width:  uint16(width),
+		Height: uint16(height),
+	})
+}
+
+type TerminalResizeQueue interface {
+	remotecommand.TerminalSizeQueue
+
+	Resize(size remotecommand.TerminalSize)
+}
+
+func newTerminalSizeQueue() TerminalResizeQueue {
+	return &terminalSizeQueue{
+		resizeChan: make(chan remotecommand.TerminalSize, 1),
+	}
+}
+
+type terminalSizeQueue struct {
+	resizeChan chan remotecommand.TerminalSize
+}
+
+func (t *terminalSizeQueue) Resize(size remotecommand.TerminalSize) {
+	select {
+	// try to send the size to resizeChan, but don't block
+	case t.resizeChan <- size:
+		// send successful
+	default:
+		// unable to send / no-op
+	}
+}
+
+func (t *terminalSizeQueue) Next() *remotecommand.TerminalSize {
+	size, ok := <-t.resizeChan
+	if !ok {
+		return nil
+	}
+	return &size
 }
--- ui/src/lib/fetch.ts
+++ ui/src/lib/fetch.ts
@@ -1,9 +1,5 @@
 import {ApiHostname} from "./rest";
 
 export default function authFetch(url: string): Promise<Response> {
-    return fetch(`http://${ApiHostname()}${url}`, {
-        headers: new Headers({
-            "Authorization": (window as any).token,
-        })
-    })
+    return fetch(`http://${ApiHostname()}${url}`)
 }
