ec2ff885f180ba202f2a2cafe1de91e99fc98a8e
fabiankramm <kramm@covexo.com>
2020-11-06 08:53:01 +0100

    feat: try to fallback to in-cluster config if kube config couldn't be loaded

--- pkg/devspace/kubectl/util/util.go
+++ pkg/devspace/kubectl/util/util.go
@@ -5,9 +5,12 @@ import (
 	"github.com/devspace-cloud/devspace/pkg/util/kubeconfig"
 	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
+
+const localContext = "local"
 
 func NewClientByContext(context, namespace string, switchContext bool, kubeLoader kubeconfig.Loader) (clientcmd.ClientConfig, string, string, error) {
 	// Load new raw config
@@ -24,7 +27,18 @@ func NewClientByContext(context, namespace string, switchContext bool, kubeLoade
 	}
 
 	if len(kubeConfig.Clusters) == 0 {
-		return nil, "", "", errors.Errorf("kube config is invalid: please make sure you have an existing valid kube config")
+		// try to load in cluster config
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			return nil, "", "", errors.Errorf("kube config is invalid: please make sure you have an existing valid kube config")
+		}
+
+		rawConfig, err := ConvertRestConfigToRawConfig(config)
+		if err != nil {
+			return nil, "", "", errors.Wrap(err, "convert in cluster config")
+		}
+
+		return clientcmd.NewNonInteractiveClientConfig(*rawConfig, localContext, &clientcmd.ConfigOverrides{}, clientcmd.NewDefaultClientConfigLoadingRules()), localContext, "default", nil
 	}
 
 	// If we should use a certain kube context use that
@@ -72,4 +86,43 @@ func NewClientByContext(context, namespace string, switchContext bool, kubeLoade
 	}
 
 	return clientConfig, activeContext, activeNamespace, nil
+}
+
+func ConvertRestConfigToRawConfig(config *rest.Config) (*clientcmdapi.Config, error) {
+	contextName := localContext
+	kubeConfig := clientcmdapi.NewConfig()
+	kubeConfig.Contexts = map[string]*clientcmdapi.Context{
+		contextName: {
+			Cluster:  contextName,
+			AuthInfo: contextName,
+		},
+	}
+	kubeConfig.Clusters = map[string]*clientcmdapi.Cluster{
+		contextName: {
+			Server:                   config.Host,
+			InsecureSkipTLSVerify:    config.Insecure,
+			CertificateAuthorityData: config.CAData,
+			CertificateAuthority:     config.CAFile,
+		},
+	}
+	kubeConfig.AuthInfos = map[string]*clientcmdapi.AuthInfo{
+		contextName: {
+			Token:                 config.BearerToken,
+			TokenFile:             config.BearerTokenFile,
+			Impersonate:           config.Impersonate.UserName,
+			ImpersonateGroups:     config.Impersonate.Groups,
+			ImpersonateUserExtra:  config.Impersonate.Extra,
+			ClientCertificate:     config.CertFile,
+			ClientCertificateData: config.CertData,
+			ClientKey:             config.KeyFile,
+			ClientKeyData:         config.KeyData,
+			Username:              config.Username,
+			Password:              config.Password,
+			AuthProvider:          config.AuthProvider,
+			Exec:                  config.ExecProvider,
+		},
+	}
+	kubeConfig.CurrentContext = contextName
+	raw, err := clientcmd.NewDefaultClientConfig(*kubeConfig, &clientcmd.ConfigOverrides{}).RawConfig()
+	return &raw, err
 }
