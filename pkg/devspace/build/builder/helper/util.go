ececfc6c2a5c05d9cd58da14f8ff65e1ffcde34b
fabiankramm <kramm@covexo.com>
2020-11-24 10:08:44 +0100

    chore: improve message for restart helper error

--- pkg/devspace/build/builder/helper/util.go
+++ pkg/devspace/build/builder/helper/util.go
@@ -131,7 +131,7 @@ func RewriteDockerfile(dockerfile string, entrypoint []string, cmd []string, add
 		if len(entrypoint) == 0 {
 			if len(oldEntrypoint) == 0 {
 				if len(cmd) == 0 && len(oldCmd) == 0 {
-					return "", errors.Errorf("cannot inject restart helper into Dockerfile because neither ENTRYPOINT nor CMD was found.\n\nHow to fix this:\n- Option A: Define an ENTRYPOINT (or CMD) in your Dockerfile\n- Option B: Set `images.*.entrypoint` option in your devspace.yaml")
+					return "", errors.Errorf("cannot inject restart helper into Dockerfile because neither ENTRYPOINT nor CMD was found.\n\nHow to fix this:\n- Option A: Define an ENTRYPOINT (or CMD) in your Dockerfile\n- Option B: Set `images.*.entrypoint` option in your devspace.yaml\n- Option C: If you don't want to inject the restart helper, set `images.*.injectRestartHelper` to false")
 				}
 				log.Warn("Using CMD statement for injecting restart helper because ENTRYPOINT is missing in Dockerfile and `images.*.entrypoint` is also not configured")
 			}
