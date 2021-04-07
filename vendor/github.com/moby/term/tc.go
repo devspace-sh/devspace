b03a1937e674e951b819724558699184f979d2a3
fabiankramm <fab.kramm@googlemail.com>
2021-04-07 10:29:51 +0200

    fix: fix moby term panic for older windows versions

--- vendor/github.com/moby/term/tc.go
+++ vendor/github.com/moby/term/tc.go
@@ -3,17 +3,18 @@
 package term
 
 import (
+	"syscall"
+	"unsafe"
+
 	"golang.org/x/sys/unix"
 )
 
-func tcget(fd uintptr) (*Termios, error) {
-	p, err := unix.IoctlGetTermios(int(fd), getTermios)
-	if err != nil {
-		return nil, err
-	}
-	return p, nil
+func tcget(fd uintptr, p *Termios) syscall.Errno {
+	_, _, err := unix.Syscall(unix.SYS_IOCTL, fd, uintptr(getTermios), uintptr(unsafe.Pointer(p)))
+	return err
 }
 
-func tcset(fd uintptr, p *Termios) error {
-	return unix.IoctlSetTermios(int(fd), setTermios, p)
+func tcset(fd uintptr, p *Termios) syscall.Errno {
+	_, _, err := unix.Syscall(unix.SYS_IOCTL, fd, setTermios, uintptr(unsafe.Pointer(p)))
+	return err
 }
