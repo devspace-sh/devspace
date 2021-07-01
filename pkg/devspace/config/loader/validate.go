cd2571b05dc342a32f9e044110c872a8c114a2c5
Mauricio Walters <mauricio@nviti.us>
2021-06-30 22:10:18 -0600

    Compare ImageName on replacePods

--- pkg/devspace/config/loader/validate.go
+++ pkg/devspace/config/loader/validate.go
@@ -321,7 +321,7 @@ func isReplacePodsUnique(index int, rp *latest.ReplacePod, rps []*latest.Replace
 
 		if r.ImageSelector == rp.ImageSelector {
 			return false
-		} else if r.ImageName == rp.ReplaceImage {
+		} else if r.ImageName == rp.ImageName {
 			return false
 		} else if len(r.LabelSelector) > 0 && len(rp.LabelSelector) > 0 && strMapEquals(r.LabelSelector, rp.LabelSelector) {
 			return false
