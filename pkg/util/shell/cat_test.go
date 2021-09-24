4489da64839b58d61929adf7ee899b2efdf6e08d
pratikjagrut <26519653+pratikjagrut@users.noreply.github.com>
2021-09-24 13:03:39 +0530

    feat: add cat command

---
+++ pkg/util/shell/cat_test.go
@@ -0,0 +1,97 @@
+package shell
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"strings"
+	"testing"
+
+	"gotest.tools/assert"
+	"mvdan.cc/sh/v3/interp"
+)
+
+// this test implies to cat testFile
+func TestCat(t *testing.T) {
+	f, err := ioutil.TempFile(".", "testFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+
+	if _, err = f.WriteString("Hello DevSpace!"); err != nil {
+		t.Fatalf("Unable to write to temporary file %v", err)
+	}
+
+	expectedOutput := "Hello DevSpace!"
+	stdout := &bytes.Buffer{}
+	stdin := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	ctx := &interp.HandlerContext{
+		Dir:    ".",
+		Stdin:  stdin,
+		Stdout: stdout,
+		Stderr: stderr,
+	}
+	args := []string{
+		f.Name(),
+	}
+
+	assert.Equal(t, stdout.String(), "")
+
+	err = cat(ctx, args)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, stderr.String(), "")
+	assert.Equal(t, stdout.String(), expectedOutput)
+}
+
+// this test implies to echo Hello DevSpace! | cat
+func TestCatNoArgs(t *testing.T) {
+	stdout := &bytes.Buffer{}
+	stdin := strings.NewReader("Hello DevSpace!")
+	stderr := &bytes.Buffer{}
+	ctx := &interp.HandlerContext{
+		Stdin:  stdin,
+		Stdout: stdout,
+		Stderr: stderr,
+	}
+	args := []string{}
+	expectedOutput := "Hello DevSpace!"
+
+	assert.Equal(t, stdout.String(), "")
+
+	err := cat(ctx, args)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, stderr.String(), "")
+	assert.Equal(t, stdout.String(), expectedOutput)
+}
+
+func TestCatNoFile(t *testing.T) {
+	expectedOutput := func() string {
+		if runtime.GOOS == "windows" {
+			return "cat: open randomFile.txt: The system cannot find the file specified."
+		}
+		return "cat: open randomFile.txt: no such file or directory"
+	}()
+
+	stdout := &bytes.Buffer{}
+	stdin := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	ctx := &interp.HandlerContext{
+		Dir:    ".",
+		Stdin:  stdin,
+		Stdout: stdout,
+		Stderr: stderr,
+	}
+	args := []string{"randomFile.txt"}
+
+	err := cat(ctx, args)
+	assert.Error(t, err, expectedOutput)
+}
---
+++ pkg/util/shell/shell_test.go
@@ -0,0 +1,108 @@
+package shell
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/pkg/errors"
+	"gotest.tools/assert"
+	"mvdan.cc/sh/v3/expand"
+)
+
+type testCaseShell struct {
+	command        string
+	expectedOutput string
+}
+
+func TestShellCat(t *testing.T) {
+	file, err := ioutil.TempFile(".", "testFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	if _, err = file.WriteString("Hello DevSpace!"); err != nil {
+		t.Fatalf("Unable to write to temporary file %v", err)
+	}
+
+	testCases := []testCaseShell{
+		{
+			command:        "cat " + file.Name(),
+			expectedOutput: "Hello DevSpace!",
+		},
+		{
+			command:        "echo 123 | cat",
+			expectedOutput: "123\n",
+		},
+	}
+
+	for _, testCase := range testCases {
+		stdout := &bytes.Buffer{}
+		err := ExecuteShellCommand(testCase.command, nil, ".", stdout, nil, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, stdout.String(), testCase.expectedOutput)
+	}
+}
+
+func TestShellCatError(t *testing.T) {
+	testCases := []testCaseShell{
+		{
+			command:        "cat noFile.txt",
+			expectedOutput: "cat: noFile.txt: No such file or directory\n",
+		},
+	}
+
+	for _, testCase := range testCases {
+		stderr := &bytes.Buffer{}
+		err := ExecuteShellCommand(testCase.command, nil, ".", nil, stderr, nil)
+		if err != nil {
+			if stderr.String() != "" {
+				assert.Equal(t, stderr.String(), testCase.expectedOutput)
+			} else {
+				t.Fatal(err)
+			}
+		} else {
+			t.Fatal("FAIL: TestShellCatError")
+		}
+
+	}
+}
+
+// this test forces the cat implementation to execute
+func TestShellCatEnforce(t *testing.T) {
+	file, err := ioutil.TempFile(".", "testFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	if _, err = file.WriteString("Hello DevSpace!"); err != nil {
+		t.Fatalf("Unable to write to temporary file %v", err)
+	}
+
+	testCases := []testCaseShell{
+		{
+			command:        "cat " + file.Name(),
+			expectedOutput: "Hello DevSpace!",
+		},
+		{
+			command:        "echo 123 | cat",
+			expectedOutput: "123\n",
+		},
+	}
+	lookPathDir = func(cwd string, env expand.Environ, file string) (string, error) {
+		return "", errors.New("not found")
+	}
+	for _, testCase := range testCases {
+		stdout := &bytes.Buffer{}
+		err := ExecuteShellCommand(testCase.command, nil, ".", stdout, nil, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, stdout.String(), testCase.expectedOutput)
+	}
+}
