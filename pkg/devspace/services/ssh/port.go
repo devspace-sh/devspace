69cb5bf48508a0886534b40b40a8636ab909aa57
Fabian Kramm <fab.kramm@googlemail.com>
2022-03-31 14:52:10 +0200

    refactor: make ssh port stable if already in .ssh/config

--- pkg/devspace/services/ssh/port.go
+++ pkg/devspace/services/ssh/port.go
@@ -3,35 +3,104 @@ package ssh
 import (
 	"fmt"
 	"github.com/loft-sh/devspace/helper/util/port"
+	"github.com/loft-sh/devspace/pkg/util/log"
+	"github.com/mitchellh/go-homedir"
+	"github.com/pkg/errors"
 	"math/rand"
+	"path/filepath"
 	"sync"
 )
 
+type PortManager interface {
+	LockPort() (int, error)
+	LockSpecificPort(p int) error
+	ReleasePort(p int)
+}
+
 var (
-	portRangeStart = 10000
-	portRangeEnd   = 12000
-	portMap        = map[int]bool{}
-	portMutex      sync.Mutex
+	portManager     PortManager
+	portManagerOnce sync.Once
 )
 
-func LockPort() (int, error) {
-	portMutex.Lock()
-	defer portMutex.Unlock()
+func GetInstance(log log.Logger) PortManager {
+	portManagerOnce.Do(func() {
+		portManager = NewManager(log)
+	})
+	return portManager
+}
+
+func NewManager(log log.Logger) PortManager {
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		log.Errorf("%v", errors.Wrap(err, "get home dir"))
+	}
+
+	sshConfigPath := filepath.Join(homeDir, ".ssh", "config")
+	hosts, err := ParseDevSpaceHosts(sshConfigPath)
+	if err != nil {
+		log.Errorf("error parsing %s: %v", sshConfigPath, err)
+	}
+
+	reservedPorts := map[int]bool{}
+	for _, h := range hosts {
+		reservedPorts[h.Port] = true
+	}
+
+	return &manager{
+		reservedPorts:  reservedPorts,
+		portRangeStart: 10000,
+		portRangeEnd:   12000,
+		portMap:        map[int]bool{},
+	}
+}
+
+type manager struct {
+	m sync.Mutex
+
+	reservedPorts map[int]bool
+
+	portRangeStart int
+	portRangeEnd   int
+	portMap        map[int]bool
+}
+
+func (m *manager) LockSpecificPort(p int) error {
+	m.m.Lock()
+	defer m.m.Unlock()
+
+	if m.portMap[p] {
+		return fmt.Errorf("port %d already in use", p)
+	}
+
+	available, err := port.IsAvailable(fmt.Sprintf(":%d", p))
+	if err != nil {
+		return err
+	} else if !available {
+		return fmt.Errorf("port %d is already in use %v", p, err)
+	}
+
+	m.portMap[p] = true
+	return nil
+}
+
+func (m *manager) LockPort() (int, error) {
+	m.m.Lock()
+	defer m.m.Unlock()
 
 	var (
 		available bool
 		err       error
 	)
 	for i := 0; i < 10; i++ {
-		p := rand.Intn(portRangeEnd-portRangeStart+1) + portRangeStart
-		if portMap[p] {
+		p := rand.Intn(m.portRangeEnd-m.portRangeStart+1) + m.portRangeStart
+		if m.portMap[p] || m.reservedPorts[p] {
 			i--
 			continue
 		}
 
 		available, err = port.IsAvailable(fmt.Sprintf(":%d", p))
 		if available {
-			portMap[p] = true
+			m.portMap[p] = true
 			return p, nil
 		}
 	}
@@ -39,9 +108,9 @@ func LockPort() (int, error) {
 	return 0, fmt.Errorf("couldn't find an open port: %v", err)
 }
 
-func ReleasePort(p int) {
-	portMutex.Lock()
-	defer portMutex.Unlock()
+func (m *manager) ReleasePort(p int) {
+	m.m.Lock()
+	defer m.m.Unlock()
 
-	delete(portMap, p)
+	delete(m.portMap, p)
 }
