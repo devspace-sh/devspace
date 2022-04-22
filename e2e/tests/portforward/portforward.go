40e580589bf1327a145b5143526a270d1ed19ae8
Maahi B <tukobadnyanoba.28@gmail.com>
2022-04-22 14:46:25 +0530

    added nginx docker image

--- e2e/tests/portforward/portforward.go
+++ e2e/tests/portforward/portforward.go
@@ -69,11 +69,13 @@ var _ = DevSpaceDescribe("portforward", func() {
 			done <- devCmd.RunDefault(f)
 		}()
 
-		framework.ExpectRemoteCurlContents("ghcr.io/vaikunthawasi/portforward-ex", ns, "localhost:3000", "Hello World!")
-		framework.ExpectLocalCurlContents("http://localhost:3000", "Hello World!")
+		nginxResp := "<!DOCTYPE html>\n<html>\n<head>\n<title>Welcome to nginx!</title>\n<style>\nhtml { color-scheme: light dark; }\nbody { width: 35em; margin: 0 auto;\nfont-family: Tahoma, Verdana, Arial, sans-serif; }\n</style>\n</head>\n<body>\n<h1>Welcome to nginx!</h1>\n<p>If you see this page, the nginx web server is successfully installed and\nworking. Further configuration is required.</p>\n\n<p>For online documentation and support please refer to\n<a href=\"http://nginx.org/\">nginx.org</a>.<br/>\nCommercial support is available at\n<a href=\"http://nginx.com/\">nginx.com</a>.</p>\n\n<p><em>Thank you for using nginx.</em></p>\n</body>\n</html>"
+		framework.ExpectRemoteCurlContents("nginx", ns, "localhost", nginxResp)
+		framework.ExpectLocalCurlContents("http://localhost:3000", nginxResp)
 
-		framework.ExpectLocalCurlContents("http://localhost:8888", "Hello World!")
-		framework.ExpectRemoteCurlContents("ghcr.io/vaikunthawasi/portforward-ex", ns, "localhost:8888", "Hello World!")
+		httpServerResp := "Hello World!"
+		framework.ExpectLocalCurlContents("http://localhost:8888", httpServerResp)
+		framework.ExpectRemoteCurlContents("nginx", ns, "localhost:8888", httpServerResp)
 
 		cancel()
 		err = <-done
