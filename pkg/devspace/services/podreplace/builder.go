3342274dd7418d43cfa50ab365de9ab47eba45d3
Ryan Harris <harrisryan1@gmail.com>
2022-11-03 15:31:50 -0400

    change var name

--- pkg/devspace/services/podreplace/builder.go
+++ pkg/devspace/services/podreplace/builder.go
@@ -84,13 +84,13 @@ func buildDeployment(ctx devspacecontext.Context, name string, target runtime.Ob
 		podTemplate.Spec = *t.Spec.Template.Spec.DeepCopy()
 		podTemplate.Spec.Hostname = strings.ReplaceAll(t.Name+"-0", ".", "-")
 		for _, pvc := range t.Spec.VolumeClaimTemplates {
-			pvcName := pvc.ObjectMeta.Name
-			if pvcName == "" {
-				pvcName = "data"
+			volumeName := pvc.ObjectMeta.Name
+			if volumeName == "" {
+				volumeName = "data"
 			}
-			claimName := pvcName + "-" + t.Name + "-0"
+			claimName := volumeName + "-" + t.Name + "-0"
 			podTemplate.Spec.Volumes = append(podTemplate.Spec.Volumes, corev1.Volume{
-				Name: pvcName,
+				Name: volumeName,
 				VolumeSource: corev1.VolumeSource{
 					PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
 						ClaimName: claimName,
