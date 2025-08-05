0107e02cbb261ea2befa06330836124b6c0e36e7
Ryan Swanson <ryan.swanson@loft.sh>
2025-08-05 12:40:00 -0600

    Use the same options as the deprecated 'Dial'
    Signed-off-by: Ryan Swanson <ryan.swanson@loft.sh>

--- helper/util/conn.go
+++ helper/util/conn.go
@@ -2,19 +2,25 @@ package util
 
 import (
 	"context"
-	"google.golang.org/grpc/credentials/insecure"
 	"io"
 	"net"
 	
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/resolver"
 )
 
 // NewClientConnection creates a new client connection for the given reader and writer
 func NewClientConnection(reader io.Reader, writer io.Writer) (*grpc.ClientConn, error) {
 	pipe := NewStdStreamJoint(reader, writer, false)
 	
+	resolver.SetDefaultScheme("passthrough")
 	// Set up a connection to the server.
-	return grpc.NewClient("", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithContextDialer(func(ctx context.Context, addr string) (net.Conn, error) {
-		return pipe, nil
-	}))
+	return grpc.NewClient("",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithContextDialer(func(ctx context.Context, addr string) (net.Conn, error) {
+			return pipe, nil
+		}),
+		grpc.WithLocalDNSResolution(),
+		grpc.WithDefaultCallOptions())
 }
